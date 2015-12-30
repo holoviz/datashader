2e589c16d5ebd7144cb655715ab77a47e61fea1b
Jim Crist <crist042@umn.edu>
2015-12-30 15:07:52 -0600

    Add setup.py, requirements.txt

--- datashader/__init__.py
+++ datashader/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+__version__ = '0.0.1'
+
 from .expr import Canvas
 
 # Needed to build the backend dispatch
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import datashader
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+setup(name='datashader',
+      version=datashader.__version__,
+      description='Bin based rendering toolchain',
+      url='http://github.com/bokeh/datashader',
+      install_requires=install_requires,
+      packages=['datashader', 'datashader.backends'])
