89d43eca424020825c3a8d30ef3d3970258e765e
Jim Crist <crist042@umn.edu>
2016-01-22 10:26:47 -0600

    Add colorize transfer function
    Colorize colors record aggregates by field, mixing colors in same bin
    together by an interpolation function.
    
    Also added support for user specified callables for the `how` keyword to
    `colorize` and `interpolate`.

--- datashader/tests/test_transfer_functions.py
+++ datashader/tests/test_transfer_functions.py
@@ -25,20 +25,50 @@ nd.as_numpy(agg.c.view_scalars(agg.c.dtype.value_type))[:] = c
 def test_interpolate(attr):
     x = getattr(agg, attr)
     img = tf.interpolate(x, 'pink', 'red', how='log').img
-    sol_log = np.array([[0, 4291543295, 4289043711],
-                        [4286675711, 0, 4282268671],
-                        [4280163839, 4278190335, 0]])
-    assert (img == sol_log).all()
+    sol = np.array([[0, 4291543295, 4289043711],
+                    [4286675711, 0, 4282268671],
+                    [4280163839, 4278190335, 0]])
+    assert (img == sol).all()
     img = tf.interpolate(x, 'pink', 'red', how='cbrt').img
-    sol_cbrt = np.array([[0, 4291543295, 4289109503],
-                         [4286807295, 0, 4282399999],
-                         [4280229375, 4278190335, 0]])
-    assert (img == sol_cbrt).all()
+    sol = np.array([[0, 4291543295, 4289109503],
+                    [4286807295, 0, 4282399999],
+                    [4280229375, 4278190335, 0]])
+    assert (img == sol).all()
     img = tf.interpolate(x, 'pink', 'red', how='linear').img
-    sol_lin = np.array([[0, 4291543295, 4289306879],
-                        [4287070463, 0, 4282597631],
-                        [4280361215, 4278190335, 0]])
-    assert (img == sol_lin).all()
+    sol = np.array([[0, 4291543295, 4289306879],
+                    [4287070463, 0, 4282597631],
+                    [4280361215, 4278190335, 0]])
+    assert (img == sol).all()
+    img = tf.interpolate(x, 'pink', 'red', how=lambda x: x ** 2).img
+    sol = np.array([[0, 4291543295, 4289504255],
+                    [4287399423, 0, 4282992127],
+                    [4280624127, 4278190335, 0]])
+    assert (img == sol).all()
+
+
+def test_colorize():
+    colors = [(255, 0, 0), '#0000FF', 'orange']
+    agg = np.array([[(0, 12, 0), (3, 0, 3)],
+                    [(12, 12, 12), (24, 0, 0)]],
+                   dtype=[(i, 'uint8') for i in 'abc'])
+    agg = nd.asarray(agg)
+    img = tf.colorize(agg, colors, how='log').img
+    sol = np.array([[3137273856, 2449494783],
+                    [4266997674, 3841982719]])
+    assert (img == sol).all()
+    colors = dict(zip('abc', colors))
+    img = tf.colorize(agg, colors, how='cbrt').img
+    sol = np.array([[3070164992, 2499826431],
+                    [4283774890, 3774873855]])
+    assert (img == sol).all()
+    img = tf.colorize(agg, colors, how='linear').img
+    sol = np.array([[1660878848, 989876991],
+                    [4283774890, 2952790271]])
+    assert (img == sol).all()
+    img = tf.colorize(agg, colors, how=lambda x: x ** 2).img
+    sol = np.array([[788463616, 436228863],
+                    [4283774890, 2080375039]])
+    assert (img == sol).all()
 
 
 img1 = tf.Image(np.dstack([np.array([[255, 0], [0, 125]], 'uint8'),
