d31939371e3437297bffbf5718fe7d8b6c805fd1
James A. Bednar <jbednar@continuum.io>
2016-02-01 15:27:23 -0600

    Changed the Pipeline interface to use Glyph and Reduction objects directly for generality

--- datashader/pipeline.py
+++ datashader/pipeline.py
@@ -15,6 +15,7 @@ import param
 from . import transfer_functions as tf
 from . import reductions
 from . import core
+from . import glyphs
 
 
 class Interpolate(param.Parameterized):
@@ -37,64 +38,63 @@ class Interpolate(param.Parameterized):
 
 class DatashaderPipeline(param.Parameterized):
     """
-    Configurable datashading pipeline.  Allows each element of the pipeline
-    to be specified independently without code duplication, e.g. to show the
-    effect of varying that element while keeping the rest of the pipeline
-    constant.
+    Configurable datashading pipeline for use with Bokeh.  Allows each
+    element of the pipeline to be specified independently without code
+    duplication, e.g. to show the effect of varying that element while
+    keeping the rest of the pipeline constant.
 
-    The supported pipeline is roughly:
+    Given a dataframe-like object df, the supported pipeline is roughly:
 
     1. create canvas of the requested size (in data space) and resolution
-    2. aggregate using the specified x and y fields, aggregate field, and agg_fn
-    3. apply specified transfer_fns, if any, in order
-    4. apply specified color_fn to translate each resulting aggregate into a color
+    2. aggregate df into pixel-shaped bins using the glyph and agg specifications
+    3. apply specified transfer_fns, if any, in order, on the set of bin values
+    4. apply specified color_fn to translate each resulting bin value into a color
     5. return the result as an image
     """
 
-    data = param.Parameter(doc="""
+    df = param.Parameter(doc="""
         Object supporting columnar-style data access, such as a Pandas
         dataframe.""")
 
-    x = param.String("x", doc="""
-        Name of the field in the supplied data object to use for the x coordinate.""")
+    glyph = param.ClassSelector(glyphs.Glyph,default=glyphs.Point("x","y"), doc="""
+        Marker shape for each point, specified using fields for the data object.""")
 
-    y = param.String("y", doc="""
-        Name of the field in the supplied data object to use for the y coordinate.""")
-
-    agg = param.String("count", doc="""
-        Name of the field in the supplied data object to use for aggregation.""")
-
-    agg_fn = param.Callable(reductions.count, doc="""
-        Function for aggregating pixel-bin contents into a scalar.""")
+    agg = param.ClassSelector(reductions.Reduction, default=reductions.count("count"), 
+        doc="""Function for incrementally reducing a bin's values into a scalar.""")
 
     transfer_fns = param.HookList(default=[], doc="""
-        Optional function(s) to apply to the aggregate after it has been created
-        and before it has been converted into a color.""")
+        Optional function(s) to apply to the aggregated bin values, before
+        they each get converted into a color.""")
 
     color_fn = param.Callable(default=Interpolate(), doc="""
-        Function to convert a scalar value into a color.""")
+        Function to convert a scalar aggregated bin value into a color.""")
 
 
     def __call__(self, plot, ranges, **params):
         """
         Accepts a Bokeh plot and a viewport specified via a ranges dictionary
-        (which should contain x_range, y_range, h, and w).  Returns an image
-        rendered at the specified location, using the current parameter values.
+        (which should contain x_range and y_range specifying the viewport in
+        data space, and h and w specifying the resolution).
+
+        Returns an image of the specified height and width, rendered
+        over the specified range in data space, using the current
+        parameter values.
         """
         ps = param.ParamOverrides(self,params)
 
         x_range, y_range = ranges['x_range'], ranges['y_range']
         h, w = ranges['h'], ranges['w']
 
-        cvs = core.Canvas(plot_width=w, plot_height=h,
-                          x_range=x_range, y_range=y_range)
+        canvas = core.Canvas(plot_width=w, plot_height=h,
+                             x_range=x_range, y_range=y_range)
 
-        agg = cvs.points(ps.data, ps.x, ps.y, ps.agg_fn(ps.agg))
+        bins = core.bypixel(ps.df, canvas, ps.glyph, ps.agg)
+
         for f in ps.transfer_fns:
-            agg = f(agg)
-        pix = ps.color_fn(agg)
+            agg = f(bins)
+        pixels = ps.color_fn(bins)
 
         dh = y_range[1] - y_range[0]
         dw = x_range[1] - x_range[0]
-        plot.image_rgba(image=[pix.img], x=x_range[0], y=y_range[0],
+        plot.image_rgba(image=[pixels.img], x=x_range[0], y=y_range[0],
                         dw=dw, dh=dh, dilate=False)
