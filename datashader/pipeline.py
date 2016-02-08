7cd154aa890a3b46dc0e160c58a920194878da83
Jim Crist <crist042@umn.edu>
2016-02-08 13:26:14 -0600

    Tests and cleanup for Pipeline
    - Use numpydoc standard for documentation
    - Add tests
    - Remove param dependency
    - Update notebooks for slight change in call signature

--- datashader/pipeline.py
+++ datashader/pipeline.py
@@ -1,77 +1,58 @@
 from __future__ import absolute_import, division, print_function
 
-"""
-Declarative interface to Datashader.
-
-Provides a configurable pipeline that makes it more convenient to
-specify individual stages independently from the others.  Does not
-cover all possible datashader functionality, and does not add any new
-underlying capabilities; all functionality is also available using the
-default imperative interface provided by other files.
-"""
-
-import param
+from toolz import identity
 
 from . import transfer_functions as tf
 from . import reductions
 from . import core
-from . import glyphs
 
-class Pipeline(param.Parameterized):
+
+class Pipeline(object):
+    """A datashading pipeline callback.
+
+    Given a declarative specification, creates a callable with the following
+    signature:
+
+    ``callback(x_range, y_range, width, height)``
+
+    where ``x_range`` and ``y_range`` form the bounding box on the viewport,
+    and ``width`` and ``height`` specify the output image dimensions.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame, dask.DataFrame
+    glyph : Glyph
+        The glyph to bin by.
+    agg : Reduction, optional
+        The reduction to compute per-pixel. Default is ``count()``.
+    transform_fn : callable, optional
+        A callable that takes the computed aggregate as an argument, and
+        returns another aggregate. This can be used to do preprocessing before
+        passing to the ``color_fn`` function.
+    color_fn : callable, optional
+        A callable that takes the output of ``tranform_fn``, and returns an
+        ``Image`` object. Default is ``interpolate``.
     """
-    Configurable datashading pipeline.  Allows each element of the
-    pipeline to be specified independently without code duplication,
-    e.g. to show the effect of varying that element while keeping the
-    rest of the pipeline constant.
+    def __init__(self, df, glyph, agg=reductions.count(),
+                 transform_fn=identity, color_fn=tf.interpolate):
+        self.df = df
+        self.glyph = glyph
+        self.agg = agg
+        self.transform_fn = transform_fn
+        self.color_fn = color_fn
 
-    Given a dataframe-like object df, the supported pipeline is roughly:
+    def __call__(self, x_range, y_range, width, height):
+        """Compute an image from the specified pipeline.
 
-    1. create canvas of the requested size (in data space) and resolution
-    2. aggregate df into pixel-shaped bins using the glyph and agg specifications
-    3. apply specified transfer_fns, if any, in order, on the set of bin values
-    4. apply specified color_fn to translate each resulting bin value into a color
-    5. return the result as an image
-    """
-
-    df = param.Parameter(doc="""
-        Object supporting columnar-style data access, such as a Pandas
-        dataframe.""")
-
-    glyph = param.ClassSelector(glyphs.Glyph,default=glyphs.Point("x","y"), doc="""
-        Marker shape for each point, specified using fields for the data object.""")
-
-    agg = param.ClassSelector(reductions.Reduction, default=reductions.count(), 
-        doc="""Function for incrementally reducing a bin's values into a scalar.""")
-
-    transfer_fns = param.HookList(default=[], doc="""
-        Optional function(s) to apply to the aggregated bin values, before
-        they each get converted into a color.""")
-
-    color_fn = param.Callable(default=tf.interpolate, doc="""
-        Function to convert a scalar aggregated bin value into a color.""")
-
-
-    def __call__(self, x_range, y_range, w, h, **params):
+        Parameters
+        ----------
+        x_range, y_range : tuple
+            The bounding box on the viewport, specified as tuples of
+            ``(min, max)``
+        width, height : int
+            The shape of the image
         """
-        Accepts a viewport in data space specified as 
-        x_range (xmin,xmax), y_range (ymin,ymax), and a rendering
-        resolution w x h.
-
-        Returns an image of the specified height and width, rendered
-        over the specified range in data space, using the current
-        parameter values.
-        """
-        ps = param.ParamOverrides(self,params)
-
-        canvas = core.Canvas(plot_width=w, plot_height=h,
+        canvas = core.Canvas(plot_width=width, plot_height=height,
                              x_range=x_range, y_range=y_range)
-
-        bins = core.bypixel(ps.df, canvas, ps.glyph, ps.agg)
-
-        for f in ps.transfer_fns:
-            agg = f(bins)
-
-        pixels = ps.color_fn(bins)
-
-        return pixels
-
+        bins = core.bypixel(self.df, canvas, self.glyph, self.agg)
+        return self.color_fn(self.transform_fn(bins))
---
+++ datashader/tests/test_pipeline.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import datashader as ds
+import datashader.transfer_functions as tf
+
+
+df = pd.DataFrame({'x': np.array(([0.] * 10 + [1] * 10)),
+                   'y': np.array(([0.] * 5 + [1] * 5 + [0] * 5 + [1] * 5)),
+                   'f64': np.arange(20, dtype='f8')})
+df.f64.iloc[2] = np.nan
+
+cvs = ds.Canvas(plot_width=2, plot_height=2, x_range=(0, 1), y_range=(0, 1))
+
+
+def test_pipeline():
+    pipeline = ds.Pipeline(df, ds.Point('x', 'y'))
+    img = pipeline((0, 1), (0, 1), 2, 2)
+    agg = cvs.points(df, 'x', 'y', ds.count())
+    assert img.equals(tf.interpolate(agg))
+
+    color_fn = lambda agg: tf.interpolate(agg, 'pink', 'red')
+    pipeline.color_fn = color_fn
+    img = pipeline((0, 1), (0, 1), 2, 2)
+    assert img.equals(color_fn(agg))
+
+    transform_fn = lambda agg: agg + 1
+    pipeline.transform_fn = transform_fn
+    img = pipeline((0, 1), (0, 1), 2, 2)
+    assert img.equals(color_fn(transform_fn(agg)))
+
+    pipeline = ds.Pipeline(df, ds.Point('x', 'y'), ds.sum('f64'))
+    img = pipeline((0, 1), (0, 1), 2, 2)
+    agg = cvs.points(df, 'x', 'y', ds.sum('f64'))
+    assert img.equals(tf.interpolate(agg))
