b6c5f04bb90f3227b7541b2305cfc6052109fd7f
Aron Ahmadia <aron@ahmadia.net>
2016-02-08 14:56:22 -0500

    Add conda recipe

---
+++ conda.recipe/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+BLD_DIR=`pwd`
+
+SRC_DIR=$RECIPE_DIR/..
+pushd $SRC_DIR
+
+$PYTHON setup.py --quiet install --single-version-externally-managed --record=record.txt
+
+popd
+
---
+++ conda.recipe/run_test.py
@@ -0,0 +1,1 @@
+import datashader; datashader.test()
