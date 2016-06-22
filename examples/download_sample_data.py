2031642f4a8fe463dead4ec42703e8b93cb78b09
bcollins <bcollins@continuum.io>
2016-06-22 12:38:08 -0500

    added datasets.yml and updated download script

--- examples/download_sample_data.py
+++ examples/download_sample_data.py
@@ -1,72 +1,232 @@
-"""Download data needed for the examples"""
-from __future__ import print_function
+#!/usr/bin/env python
 
-from os import path, makedirs, remove
-from progress import bar as progressbar
+from __future__ import print_function, absolute_import, division
 
-import pandas as pd
-import numpy as np
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2011, Kenneth Reitz <me@kennethreitz.com>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+clint.textui.progress
+~~~~~~~~~~~~~~~~~
+
+This module provides the progressbar functionality.
+
+"""
 import sys
+import time
+import tarfile
+import zipfile
+
+import os
+import yaml
 
 try:
     import requests
 except ImportError:
-    print('Download script required requests package: conda install requests')
+    print('this download script requires the requests module: conda install requests')
     sys.exit(1)
 
-def _download_dataset(url):
-    r = requests.get(url, stream=True)
-    output_path = path.split(url)[1]
-    with open(output_path, 'wb') as f:
-        total_length = int(r.headers.get('content-length'))
-        for chunk in progressbar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
-            if chunk:
-                f.write(chunk)
-                f.flush()
+from collections import OrderedDict
 
-examples_dir = path.dirname(path.realpath(__file__))
-data_dir = path.join(examples_dir, 'data')
-if not path.exists(data_dir):
-    makedirs(data_dir)
+from os import path
 
-# Taxi data
-def latlng_to_meters(df, lat_name, lng_name):
-    lat = df[lat_name]
-    lng = df[lng_name]
-    origin_shift = 2 * np.pi * 6378137 / 2.0
-    mx = lng * origin_shift / 180.0
-    my = np.log(np.tan((90 + lat) * np.pi / 360.0)) / (np.pi / 180.0)
-    my = my * origin_shift / 180.0
-    df.loc[:, lng_name] = mx
-    df.loc[:, lat_name] = my
+STREAM = sys.stderr
 
-taxi_path = path.join(data_dir, 'nyc_taxi.csv')
-if not path.exists(taxi_path):
-    print("Downloading Taxi Data...")
-    url = ('https://storage.googleapis.com/tlc-trip-data/2015/'
-           'yellow_tripdata_2015-01.csv')
+BAR_TEMPLATE = '%s[%s%s] %i/%i - %s\r'
+MILL_TEMPLATE = '%s %s %i/%i\r'
 
-    _download_dataset(url)
-    df = pd.read_csv('yellow_tripdata_2015-01.csv')
+DOTS_CHAR = '.'
+BAR_FILLED_CHAR = '#'
+BAR_EMPTY_CHAR = ' '
+MILL_CHARS = ['|', '/', '-', '\\']
 
-    print('Filtering Taxi Data')
-    df = df.loc[(df.pickup_longitude < -73.75) &
-                (df.pickup_longitude > -74.15) &
-                (df.dropoff_longitude < -73.75) &
-                (df.dropoff_longitude > -74.15) &
-                (df.pickup_latitude > 40.68) &
-                (df.pickup_latitude < 40.84) &
-                (df.dropoff_latitude > 40.68) &
-                (df.dropoff_latitude < 40.84)].copy()
+# How long to wait before recalculating the ETA
+ETA_INTERVAL = 1
+# How many intervals (excluding the current one) to calculate the simple moving
+# average
+ETA_SMA_WINDOW = 9
 
-    print('Reprojecting Taxi Data')
-    latlng_to_meters(df, 'pickup_latitude', 'pickup_longitude')
-    latlng_to_meters(df, 'dropoff_latitude', 'dropoff_longitude')
-    df.rename(columns={'pickup_longitude': 'pickup_x', 'dropoff_longitude': 'dropoff_x',
-                       'pickup_latitude': 'pickup_y', 'dropoff_latitude': 'dropoff_y'},
-              inplace=True)
-    df.to_csv(taxi_path, index=False)
-    remove('yellow_tripdata_2015-01.csv')
-    
 
-print("\nAll data downloaded.")
+class Bar(object):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.done()
+        return False  # we're not suppressing exceptions
+
+    def __init__(self, label='', width=32, hide=None, empty_char=BAR_EMPTY_CHAR,
+                 filled_char=BAR_FILLED_CHAR, expected_size=None, every=1):
+        self.label = label
+        self.width = width
+        self.hide = hide
+        # Only show bar in terminals by default (better for piping, logging etc.)
+        if hide is None:
+            try:
+                self.hide = not STREAM.isatty()
+            except AttributeError:  # output does not support isatty()
+                self.hide = True
+        self.empty_char =    empty_char
+        self.filled_char =   filled_char
+        self.expected_size = expected_size
+        self.every =         every
+        self.start =         time.time()
+        self.ittimes =       []
+        self.eta =           0
+        self.etadelta =      time.time()
+        self.etadisp =       self.format_time(self.eta)
+        self.last_progress = 0
+        if (self.expected_size):
+            self.show(0)
+
+    def show(self, progress, count=None):
+        if count is not None:
+            self.expected_size = count
+        if self.expected_size is None:
+            raise Exception("expected_size not initialized")
+        self.last_progress = progress
+        if (time.time() - self.etadelta) > ETA_INTERVAL:
+            self.etadelta = time.time()
+            self.ittimes = \
+                self.ittimes[-ETA_SMA_WINDOW:] + \
+                    [-(self.start - time.time()) / (progress+1)]
+            self.eta = \
+                sum(self.ittimes) / float(len(self.ittimes)) * \
+                (self.expected_size - progress)
+            self.etadisp = self.format_time(self.eta)
+        x = int(self.width * progress / self.expected_size)
+        if not self.hide:
+            if ((progress % self.every) == 0 or      # True every "every" updates
+                (progress == self.expected_size)):   # And when we're done
+                STREAM.write(BAR_TEMPLATE % (
+                    self.label, self.filled_char * x,
+                    self.empty_char * (self.width - x), progress,
+                    self.expected_size, self.etadisp))
+                STREAM.flush()
+
+    def done(self):
+        self.elapsed = time.time() - self.start
+        elapsed_disp = self.format_time(self.elapsed)
+        if not self.hide:
+            # Print completed bar with elapsed time
+            STREAM.write(BAR_TEMPLATE % (
+                self.label, self.filled_char * self.width,
+                self.empty_char * 0, self.last_progress,
+                self.expected_size, elapsed_disp))
+            STREAM.write('\n')
+            STREAM.flush()
+
+    def format_time(self, seconds):
+        return time.strftime('%H:%M:%S', time.gmtime(seconds))
+
+
+def bar(it, label='', width=32, hide=None, empty_char=BAR_EMPTY_CHAR,
+        filled_char=BAR_FILLED_CHAR, expected_size=None, every=1):
+    """Progress iterator. Wrap your iterables with it."""
+
+    count = len(it) if expected_size is None else expected_size
+
+    with Bar(label=label, width=width, hide=hide, empty_char=BAR_EMPTY_CHAR,
+             filled_char=BAR_FILLED_CHAR, expected_size=count, every=every) \
+            as bar:
+        for i, item in enumerate(it):
+            yield item
+            bar.show(i + 1)
+
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+
+class DirectoryContext(object):
+    """
+    Context Manager for changing directories
+    """
+    def __init__(self, path):
+        self.old_dir = os.getcwd()
+        self.new_dir = path
+ 
+    def __enter__(self):
+        os.chdir(self.new_dir)
+ 
+    def __exit__(self, *args):
+        os.chdir(self.old_dir)
+
+def _process_dataset(dataset, output_dir):
+
+    if not path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    with DirectoryContext(output_dir) as d:
+        
+        requires_download = False
+
+        for f in dataset.get('files', []):
+            if not path.exists(f):
+                requires_download = True
+                break
+
+        if not requires_download:
+            print('Skipping {0}'.format(dataset['title']))
+            return
+
+
+        print('Downloading {0}'.format(dataset['title']))
+        r = requests.get(dataset['url'], stream=True)
+        output_path = path.split(dataset['url'])[1]
+        with open(output_path, 'wb') as f:
+            total_length = int(r.headers.get('content-length'))
+            for chunk in bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+
+        # extract content
+        if output_path.endswith("tar.gz"):
+            with tarfile.open(output_path, "r:gz") as tar:
+                tar.extractall()
+            os.remove(output_path)
+        elif output_path.endswith("tar"):
+            with tarfile.open(output_path, "r:") as tar:
+                tar.extractall()
+            os.remove(output_path)
+        elif output_path.endswith("tar.bz2"):
+            with tarfile.open(output_path, "r:bz2") as tar:
+                tar.extractall()
+            os.remove(output_path)
+        elif output_path.endswith("zip"):
+            with zipfile.ZipFile(output_path, 'r') as zipf:
+                zipf.extractall()
+            os.remove(output_path)
+
+def main():
+
+    here = contrib_dir = path.abspath(path.join(path.split(__file__)[0]))
+    info_file = path.join(here, 'datasets.yml')
+    with open(info_file) as f:
+        info = ordered_load(f.read())
+        for topic, downloads in info.items():
+            output_dir = path.join(here, topic)
+            for d in downloads:
+                _process_dataset(d, output_dir)
+
+if __name__ == '__main__':
+    main()
