f5612ff5d3b23759704c8ecd0add8fd50d4866f9
James A. Bednar <jbednar@continuum.io>
2016-09-08 16:08:42 -0500

    Removed confusing "missing" argument for rasters

--- datashader/tests/test_raster.py
+++ datashader/tests/test_raster.py
@@ -38,18 +38,13 @@ def test_raster_aggregate_without_overviews():
         agg = cvs.raster(src, use_overviews=False)
         assert agg is not None
 
-def test_raster_set_missing():
-    with rio.open(TEST_RASTER_PATH) as src:
-        agg = cvs.raster(src, missing=-100)
-        assert agg is not None
-
 def test_out_of_bounds_return_correct_size():
     with rio.open(TEST_RASTER_PATH) as src:
         cvs = ds.Canvas(plot_width=2,
                         plot_height=2,
                         x_range=[1e10, 1e20],
                         y_range=[1e10, 1e20])
-        agg = cvs.raster(src, missing=-100)
+        agg = cvs.raster(src)
         assert agg.shape == (2,2)
         assert agg is not None
 
@@ -61,7 +56,7 @@ def test_partial_extent_returns_correct_size():
                         plot_height=256,
                         x_range=[src.bounds.left-half_width, src.bounds.left+half_width],
                         y_range=[src.bounds.bottom-half_height, src.bounds.bottom+half_height])
-        agg = cvs.raster(src, missing=-100)
+        agg = cvs.raster(src)
         assert agg.shape == (256, 512)
         assert agg is not None
 
