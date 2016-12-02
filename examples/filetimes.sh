1f1b690df3149b1879020d918f626cedd1701948
James A. Bednar <jbednar@continuum.io>
2016-12-02 13:34:37 -0600

    Ensured that pip fetches latest repos for filetimes

--- examples/filetimes.sh
+++ examples/filetimes.sh
@@ -3,9 +3,9 @@
 #    conda-env create -f filetimes.yml
 #    source activate filetimes
 #    conda remove dask fastparquet
-#    pip install git+git://github.com/dask/dask.git@b6ba65dd788581f3bb589684f4f09c8c92f43607
-#    pip install git+git://github.com/dask/fastparquet.git@66c8c445f054e96d4ada41d330385c19880996f1
-#    pip install git+git://github.com/blaze/castra.git@1ae53dfcafdd469f0df4620172bf7f6dffb3d5dd
+#    pip install --upgrade git+git://github.com/dask/dask.git@b6ba65dd788581f3bb589684f4f09c8c92f43607
+#    pip install --upgrade git+git://github.com/dask/fastparquet.git@446edefcdc0cca785ed3d9039d5668f77e0e580c
+#    pip install --upgrade git+git://github.com/blaze/castra.git@1ae53dfcafdd469f0df4620172bf7f6dffb3d5dd
 #    mkdir times
 #    python -c "import filetimes ; filetimes.base='census' ; filetimes.categories=['race']; filetimes.timed_write('data/tinycensus.csv')" # or census.h5
 #    ./filetimes.sh times/tinycensus
