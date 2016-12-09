128592b27d1dad8b0e02db493e200ba64d777734
James A. Bednar <jbednar@continuum.io>
2016-12-09 15:40:51 -0600

    Made lnglat_to_meters accept tuples and lists as input

--- datashader/utils.py
+++ datashader/utils.py
@@ -142,7 +142,8 @@ def lnglat_to_meters(longitude,latitude):
     coordinates (meters East of Greenwich and meters North of the Equator).
 
     Longitude and latitude can be provided as scalars, Pandas columns,
-    or Numpy arrays, and will be returned in the same form.
+    or Numpy arrays, and will be returned in the same form.  Lists
+    or tuples will be converted to Numpy arrays.
     
     Examples:
        easting, northing = lnglat_to_meters(-40.71,74)
@@ -152,6 +153,9 @@ def lnglat_to_meters(longitude,latitude):
        df=pandas.DataFrame(dict(longitude=np.array([-74]),latitude=np.array([40.71])))
        df.loc[:, 'longitude'], df.loc[:, 'latitude'] = lnglat_to_meters(df.longitude,df.latitude) 
     """
+    if isinstance(longitude, (list, tuple)): longitude=np.array(longitude)
+    if isinstance(latitude,  (list, tuple)): latitude =np.array(latitude)
+
     origin_shift = np.pi * 6378137
     easting   = longitude * origin_shift / 180.0
     northing  = np.log(np.tan((90 + latitude) * np.pi / 360.0)) * origin_shift / np.pi
