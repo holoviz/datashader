83820878b11c41deede538600fa00a27e43b2b55
James A. Bednar <jbednar@continuum.io>
2016-12-22 16:33:56 -0600

    Updated versions for filetimes

--- examples/filetimes.sh
+++ examples/filetimes.sh
@@ -2,8 +2,8 @@
 # Usage:
 #    conda-env create -f filetimes.yml
 #    source activate filetimes
-#    pip install --upgrade git+git://github.com/dask/dask.git@4ae1a203860bd67f09057778c0da5af533b9fcc6
-#    pip install --upgrade git+git://github.com/dask/fastparquet.git@ea1680d99ee664905b1c006117411ac4e8a9dae0
+#    pip install --upgrade git+git://github.com/dask/dask.git@54ae465584409ac70eb022c957540a40397e60c3
+#    pip install --upgrade git+git://github.com/dask/fastparquet.git@3a57c8335999f311510b375371008e025a0498fc
 #    pip install --upgrade git+git://github.com/numba/numba.git@c80e0a3dfe86c99c474a5fbe7f657b0bab26ada2
 #    pip install --upgrade git+git://github.com/blaze/castra.git@1ae53dfcafdd469f0df4620172bf7f6dffb3d5dd
 #    pip install --upgrade git+git://github.com/bokeh/datashader.git@f69047ebb762431e14ef87ba43272e5ea0860d0f
