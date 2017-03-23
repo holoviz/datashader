7bdf31f1ef7b91154d3f2dcbff6de2fa3d5f3146
gbrener <gbrener@continuum.io>
2017-03-23 13:47:23 -0500

    Update filetimes benchmarks
    Update filetimes.yml to use latest libraries, add fastparquet patch, adjust instructions in filetimes.sh.

--- examples/filetimes.sh
+++ examples/filetimes.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 # Usage:
 #    conda-env create -f filetimes.yml
+#    patch --verbose -b -p4 -d $CONDA_PREFIX/lib/python3.5/site-packages/fastparquet < fastparquet.type_mismatch.patch
 #    source activate filetimes
-#    pip install --upgrade git+git://github.com/dask/dask.git@54ae465584409ac70eb022c957540a40397e60c3
-#    pip install --upgrade git+git://github.com/dask/fastparquet.git@3a57c8335999f311510b375371008e025a0498fc
-#    pip install --upgrade git+git://github.com/numba/numba.git@c80e0a3dfe86c99c474a5fbe7f657b0bab26ada2
-#    pip install --upgrade git+git://github.com/blaze/castra.git@1ae53dfcafdd469f0df4620172bf7f6dffb3d5dd
-#    pip install --upgrade git+git://github.com/bokeh/datashader.git@f69047ebb762431e14ef87ba43272e5ea0860d0f
 #    mkdir times
 #    python -c "import filetimes as ft ; ft.p.base='census' ; ft.p.x='meterswest' ; ft.p.y='metersnorth' ; ft.p.categories=['race']; ft.timed_write('data/tinycensus.csv',dftype='pandas')"
 #    # (or 'data/census.h5' and/or dftype='dask')
@@ -19,8 +15,8 @@ ${timer} python filetimes.py ${1}.parq        dask    census meterswest metersno
 ${timer} python filetimes.py ${1}.snappy.parq dask    census meterswest metersnorth race
 ${timer} python filetimes.py ${1}.parq        pandas  census meterswest metersnorth race
 ${timer} python filetimes.py ${1}.snappy.parq pandas  census meterswest metersnorth race
-${timer} python filetimes.py ${1}.castra      dask    census meterswest metersnorth race
-#${timer} python filetimes.py ${1}.castra      pandas  census meterswest metersnorth race
+#${timer} python filetimes.py ${1}.castra      dask    census meterswest metersnorth race
+${timer} python filetimes.py ${1}.castra      pandas  census meterswest metersnorth race
 ${timer} python filetimes.py ${1}.bcolz       dask    census meterswest metersnorth race
 ${timer} python filetimes.py ${1}.h5          dask    census meterswest metersnorth race
 ${timer} python filetimes.py ${1}.h5          pandas  census meterswest metersnorth race
