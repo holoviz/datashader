Fix off-by-one error
Fix #259.

datashader/tests/test_pandas.py
@@ -4,6 +4,8 @@ import xarray as xr
 
 import datashader as ds
 
+import pytest
+
 
 df = pd.DataFrame({'x': np.array(([0.] * 10 + [1] * 10)),
                    'y': np.array(([0.] * 5 + [1] * 5 + [0] * 5 + [1] * 5)),
@@ -19,13 +21,13 @@ df.cat = df.cat.astype('category')
 df.f32[2] = np.nan
 df.f64[2] = np.nan
 
-c = ds.Canvas(plot_width=2, plot_height=2, x_range=(0, 1), y_range=(0, 1))
-c_logx = ds.Canvas(plot_width=2, plot_height=2, x_range=(1, 10),
-                   y_range=(0, 1), x_axis_type='log')
-c_logy = ds.Canvas(plot_width=2, plot_height=2, x_range=(0, 1),
-                   y_range=(1, 10), y_axis_type='log')
-c_logxy = ds.Canvas(plot_width=2, plot_height=2, x_range=(1, 10),
-                    y_range=(1, 10), x_axis_type='log', y_axis_type='log')
+c = ds.Canvas(plot_width=2, plot_height=2, x_range=(0, 2), y_range=(0, 2))
+c_logx = ds.Canvas(plot_width=2, plot_height=2, x_range=(1, 11),
+                   y_range=(0, 2), x_axis_type='log')
+c_logy = ds.Canvas(plot_width=2, plot_height=2, x_range=(0, 2),
+                   y_range=(1, 11), y_axis_type='log')
+c_logxy = ds.Canvas(plot_width=2, plot_height=2, x_range=(1, 11),
+                    y_range=(1, 11), x_axis_type='log', y_axis_type='log')
 
 coords = [np.arange(2, dtype='f8'), np.arange(2, dtype='f8')]
 dims = ['y_axis', 'x_axis']
@@ -146,14 +148,15 @@ def test_multiple_aggregates():
 
 
 def test_log_axis():
+    x_max_index = 10 ** (1 / (2 / np.log10(11)))
     sol = np.array([[5, 5], [5, 5]], dtype='i4')
-    out = xr.DataArray(sol, coords=[np.array([0., 1.]), np.array([1., 10.])],
+    out = xr.DataArray(sol, coords=[np.array([0., 1.]), np.array([1., x_max_index])],
                        dims=dims)
     assert_eq(c_logx.points(df, 'log_x', 'y', ds.count('i32')), out)
-    out = xr.DataArray(sol, coords=[np.array([1., 10.]), np.array([0., 1.])],
+    out = xr.DataArray(sol, coords=[np.array([1., x_max_index]), np.array([0., 1.])],
                        dims=dims)
     assert_eq(c_logy.points(df, 'x', 'log_y', ds.count('i32')), out)
-    out = xr.DataArray(sol, coords=[np.array([1., 10.]), np.array([1., 10.])],
+    out = xr.DataArray(sol, coords=[np.array([1., x_max_index]), np.array([1., x_max_index])],
                        dims=dims)
     assert_eq(c_logxy.points(df, 'log_x', 'log_y', ds.count('i32')), out)
 
@@ -162,15 +165,15 @@ def test_line():
     df = pd.DataFrame({'x': [4, 0, -4, -3, -2, -1.9, 0, 10, 10, 0, 4],
                        'y': [0, -4, 0, 1, 2, 2.1, 4, 20, 30, 4, 0]})
     cvs = ds.Canvas(plot_width=7, plot_height=7,
-                    x_range=(-3, 3), y_range=(-3, 3))
+                    x_range=(-3, 4), y_range=(-3, 4))
     agg = cvs.line(df, 'x', 'y', ds.count())
     sol = np.array([[0, 0, 1, 0, 1, 0, 0],
                     [0, 1, 0, 0, 0, 1, 0],
                     [1, 0, 0, 0, 0, 0, 1],
                     [0, 0, 0, 0, 0, 0, 0],
-                    [1, 0, 0, 0, 0, 0, 1],
+                    [3, 0, 0, 0, 0, 0, 1],
                     [0, 2, 0, 0, 0, 1, 0],
                     [0, 0, 1, 0, 1, 0, 0]], dtype='i4')
-    out = xr.DataArray(sol, coords=[np.arange(-3, 4), np.arange(-3, 4)],
+    out = xr.DataArray(sol, coords=[np.arange(-3., 4.), np.arange(-3., 4.)],
                        dims=['y_axis', 'x_axis'])
     assert_eq(agg, out)