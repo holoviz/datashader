e22331c8efcf79975188fd175e6cbbc4de9523c0
Joseph Crail <jbcrail@gmail.com>
2017-04-28 22:16:43 -0400

    Fix copyright

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'datashader'
-copyright = u'2016, datashader contributors'
+copyright = u'2016, Continuum Analytics'
 author = u'datashader contributors'
 
 # The version info for the project you're documenting, acts as replacement for
