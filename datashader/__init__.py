fd7d383d54db395b51d35426ee0d139020feb27f
Joseph Crail <jbcrail@gmail.com>
2017-05-11 22:14:35 -0400

    Bump version to 0.5.0

--- datashader/__init__.py
+++ datashader/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = '0.5.0dev1'
+__version__ = '0.5.0'
 
 from .core import Canvas
 from .reductions import (count, any, sum, min, max, mean, std, var, count_cat,
