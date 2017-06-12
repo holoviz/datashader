0b77bdb73006707a9ef1a3a11f11aa639e4d7276
gbrener <gbrener@continuum.io>
2017-06-12 00:34:17 -0500

    Update tests to use xarray instead of rasterio
    Notebooks will be updated shortly to follow suit.

--- datashader/tests/test_raster.py
+++ datashader/tests/test_raster.py
@@ -2,15 +2,15 @@ from os import path
 
 import pytest
 import datashader as ds
-import rasterio as rio
+import xarray as xr
 
 BASE_PATH = path.split(__file__)[0]
 DATA_PATH = path.abspath(path.join(BASE_PATH, 'data'))
 TEST_RASTER_PATH = path.join(DATA_PATH, 'world.rgb.tif')
 
-with rio.open(TEST_RASTER_PATH) as src:
-    x_range = (src.bounds.left, src.bounds.right)
-    y_range = (src.bounds.bottom, src.bounds.top)
+with xr.open_rasterio(TEST_RASTER_PATH) as src:
+    x_range = (src._file_obj.bounds.left, src._file_obj.bounds.right)
+    y_range = (src._file_obj.bounds.bottom, src._file_obj.bounds.top)
     cvs = ds.Canvas(plot_width=2,
                     plot_height=2,
                     x_range=x_range,
@@ -18,31 +18,31 @@ with rio.open(TEST_RASTER_PATH) as src:
 
 
 def test_raster_aggregate_default():
-    with rio.open(TEST_RASTER_PATH) as src:
+    with xr.open_rasterio(TEST_RASTER_PATH) as src:
         agg = cvs.raster(src)
         assert agg is not None
 
 
 def test_raster_aggregate_nearest():
-    with rio.open(TEST_RASTER_PATH) as src:
+    with xr.open_rasterio(TEST_RASTER_PATH) as src:
         agg = cvs.raster(src, resample_method='nearest')
         assert agg is not None
 
 
 def test_raster_aggregate_with_overviews():
-    with rio.open(TEST_RASTER_PATH) as src:
+    with xr.open_rasterio(TEST_RASTER_PATH) as src:
         agg = cvs.raster(src, use_overviews=True)
         assert agg is not None
 
 
 def test_raster_aggregate_without_overviews():
-    with rio.open(TEST_RASTER_PATH) as src:
+    with xr.open_rasterio(TEST_RASTER_PATH) as src:
         agg = cvs.raster(src, use_overviews=False)
         assert agg is not None
 
 
 def test_out_of_bounds_return_correct_size():
-    with rio.open(TEST_RASTER_PATH) as src:
+    with xr.open_rasterio(TEST_RASTER_PATH) as src:
         cvs = ds.Canvas(plot_width=2,
                         plot_height=2,
                         x_range=[1e10, 1e20],
@@ -53,13 +53,13 @@ def test_out_of_bounds_return_correct_size():
 
 
 def test_partial_extent_returns_correct_size():
-    with rio.open(TEST_RASTER_PATH) as src:
-        half_width = (src.bounds.right - src.bounds.left) / 2
-        half_height = (src.bounds.top - src.bounds.bottom) / 2
+    with xr.open_rasterio(TEST_RASTER_PATH) as src:
+        half_width = (src._file_obj.bounds.right - src._file_obj.bounds.left) / 2
+        half_height = (src._file_obj.bounds.top - src._file_obj.bounds.bottom) / 2
         cvs = ds.Canvas(plot_width=512,
                         plot_height=256,
-                        x_range=[src.bounds.left-half_width, src.bounds.left+half_width],
-                        y_range=[src.bounds.bottom-half_height, src.bounds.bottom+half_height])
+                        x_range=[src._file_obj.bounds.left-half_width, src._file_obj.bounds.left+half_width],
+                        y_range=[src._file_obj.bounds.bottom-half_height, src._file_obj.bounds.bottom+half_height])
         agg = cvs.raster(src)
         assert agg.shape == (256, 512)
         assert agg is not None
