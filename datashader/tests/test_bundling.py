c65c22227c00e2408e7ee381fb114b3cf4f21fcc
Joseph Crail <jbcrail@gmail.com>
2017-06-28 11:48:24 -0400

    Reduce memory profile of weighted edges
    Previously I add a weight column with a constant value if the required
    internal dataframe did not have a weight column. For directly connected
    edges and bundled edges without a weight column, this caused the memory
    profile to increase.
    
    I refactored the code to reduce this profile and pass the necessary
    parameters to the other helper methods.

--- datashader/tests/test_bundling.py
+++ datashader/tests/test_bundling.py
@@ -29,7 +29,19 @@ def edges():
     return edges_df
 
 
-def test_directly_connect(nodes, edges):
+@pytest.fixture
+def weighted_edges():
+    # Four weighted edges originating from the center node and connected
+    # to each corner
+    edges_df = pd.DataFrame({'id': np.arange(4),
+                             'source': np.zeros(4),
+                             'target': np.arange(1, 5),
+                             'weight': np.ones(4)})
+    edges_df.set_index('id')
+    return edges_df
+
+
+def test_directly_connect_with_weights(nodes, weighted_edges):
     # Expect four lines starting at center (0.5, 0.5) and terminating
     # at a different corner and NaN
     data = pd.DataFrame({'x':
@@ -37,17 +49,29 @@ def test_directly_connect(nodes, edges):
                              0.5, 0.0, np.nan, 0.5, 1.0, np.nan],
                          'y':
                             [0.5, 1.0, np.nan, 0.5, 1.0, np.nan,
-                             0.5, 0.0, np.nan, 0.5, 0.0, np.nan],
-                         'weight':
-                            [1.0, 1.0, np.nan, 1.0, 1.0, np.nan,
-                             1.0, 1.0, np.nan, 1.0, 1.0, np.nan]})
-    expected = pd.DataFrame(data, columns=['x', 'y', 'weight'])
+                             0.5, 0.0, np.nan, 0.5, 0.0, np.nan]})
+    expected = pd.DataFrame(data, columns=['x', 'y'])
+
+    given = directly_connect_edges(nodes, weighted_edges)
+    assert given.equals(expected)
+
+
+def test_directly_connect_without_weights(nodes, edges):
+    # Expect four lines starting at center (0.5, 0.5) and terminating
+    # at a different corner and NaN
+    data = pd.DataFrame({'x':
+                            [0.5, 0.0, np.nan, 0.5, 1.0, np.nan,
+                             0.5, 0.0, np.nan, 0.5, 1.0, np.nan],
+                         'y':
+                            [0.5, 1.0, np.nan, 0.5, 1.0, np.nan,
+                             0.5, 0.0, np.nan, 0.5, 0.0, np.nan]})
+    expected = pd.DataFrame(data, columns=['x', 'y'])
 
     given = directly_connect_edges(nodes, edges)
     assert given.equals(expected)
 
 
-def test_hammer_bundle(nodes, edges):
+def test_hammer_bundle_with_weights(nodes, weighted_edges):
     # Expect four lines starting at center (0.5, 0.5) and terminating
     # with NaN
     data = pd.DataFrame({'x':
@@ -60,6 +84,28 @@ def test_hammer_bundle(nodes, edges):
                             [1.0, np.nan, 1.0, np.nan,
                              1.0, np.nan, 1.0, np.nan]})
     expected = pd.DataFrame(data, columns=['x', 'y', 'weight'])
+
+    df = hammer_bundle(nodes, weighted_edges)
+
+    starts = df[(df.x == 0.5) & (df.y == 0.5)]
+    ends = df[df.isnull().any(axis=1)]
+    given = pd.concat([starts, ends])
+    given.sort_index(inplace=True)
+    given.reset_index(drop=True, inplace=True)
+
+    assert given.equals(expected)
+
+
+def test_hammer_bundle_without_weights(nodes, edges):
+    # Expect four lines starting at center (0.5, 0.5) and terminating
+    # with NaN
+    data = pd.DataFrame({'x':
+                            [0.5, np.nan, 0.5, np.nan,
+                             0.5, np.nan, 0.5, np.nan],
+                         'y':
+                            [0.5, np.nan, 0.5, np.nan,
+                             0.5, np.nan, 0.5, np.nan]})
+    expected = pd.DataFrame(data, columns=['x', 'y'])
 
     df = hammer_bundle(nodes, edges)
 
