c5d901841cc73925096ed7870b1bbf82566105eb
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2017-08-03 11:58:16 +0100

    Fixed out-of-bounds indexing in resampling

--- datashader/resampling.py
+++ datashader/resampling.py
@@ -317,14 +317,16 @@ def _downsample_2d(src, mask, use_mask, method, fill_value, mode_rank, out):
             src_yf1 = src_yf0 + scale_y
             src_y0 = int(src_yf0)
             src_y1 = int(src_yf1)
-            if src_y1 == src_yf1 and src_y1 > src_y0:
+            wy1 = src_yf1 - src_y1
+            if wy1 < _EPS and src_y1 > src_y0:
                 src_y1 -= 1
             for out_x in range(out_w):
                 src_xf0 = scale_x * out_x
                 src_xf1 = src_xf0 + scale_x
                 src_x0 = int(src_xf0)
                 src_x1 = int(src_xf1)
-                if src_x1 == src_xf1 and src_x1 > src_x0:
+                wx1 = src_xf1 - src_x1
+                if wx1 < _EPS and src_x1 > src_x0:
                     src_x1 -= 1
                 done = False
                 value = fill_value
@@ -345,14 +347,16 @@ def _downsample_2d(src, mask, use_mask, method, fill_value, mode_rank, out):
             src_yf1 = src_yf0 + scale_y
             src_y0 = int(src_yf0)
             src_y1 = int(src_yf1)
-            if src_y1 == src_yf1 and src_y1 > src_y0:
+            wy1 = src_yf1 - src_y1
+            if wy1 < _EPS and src_y1 > src_y0:
                 src_y1 -= 1
             for out_x in range(out_w):
                 src_xf0 = scale_x * out_x
                 src_xf1 = src_xf0 + scale_x
                 src_x0 = int(src_xf0)
                 src_x1 = int(src_xf1)
-                if src_x1 == src_xf1 and src_x1 > src_x0:
+                wx1 = src_xf1 - src_x1
+                if wx1 < _EPS and src_x1 > src_x0:
                     src_x1 -= 1
                 if method == DS_MIN:
                     value = np.inf
