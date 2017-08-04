639f62aa027478ea19989b141db07c8c281ce580
gbrener <gbrener@continuum.io>
2017-08-04 16:03:19 -0500

    Bump datashader version for dev release

--- datashader/__init__.py
+++ datashader/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = '0.6.0dev4'
+__version__ = '0.6.0dev5'
 
 from .core import Canvas                                 # noqa (API import)
 from .reductions import (count, any, sum, min, max,      # noqa (API import)
