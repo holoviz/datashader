holoviz/datashader

commit 9843c77c19571eba46f4f54b21adee6b8c431170

Ensured Image.data is uint32 to avoid issues with set_background on blank images

datashader/transfer_functions.py
@@ -179,7 +179,7 @@ def _interpolate(agg, cmap, how, alpha, span):
 
         masked = data[~mask]
         if len(masked) == 0:
-            return Image(agg.data, coords=agg.coords, dims=agg.dims, attrs=agg.attrs)
+            return Image(agg.data.view(np.uint32), coords=agg.coords, dims=agg.dims, attrs=agg.attrs)
 
         offset = masked.min()
 

commit 315b852aed6491c4b3a2b3038ac7ad0ed20b5120

Fix bug for all-NaN arrays introduced in commit 9843c77c

datashader/transfer_functions.py
@@ -128,7 +128,7 @@ def _interpolate(agg, cmap, how, alpha, span, min_alpha):
 
         masked = data[~mask]
         if len(masked) == 0:
-            return Image(agg.data.view(np.uint32), coords=agg.coords, dims=agg.dims, attrs=agg.attrs)
+            return Image(agg.data.astype(np.uint32), coords=agg.coords, dims=agg.dims, attrs=agg.attrs)
 
         offset = masked.min()
 