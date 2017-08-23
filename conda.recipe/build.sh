3156d650e153c024e18164347a0f5b36205af4e8
Joseph Crail <jbcrail@gmail.com>
2017-08-23 15:26:31 -0700

    Ignore IPython checkpoints for conda builds
    Even though we ignore checkpoints when building pip packages, those
    files are still copied in conda packages.

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -9,7 +9,10 @@ SRC_DIR=$RECIPE_DIR/..
 pushd $SRC_DIR
 
 $PYTHON setup.py --quiet install --single-version-externally-managed --record=record.txt
+
+# Copy all examples files but exclude IPython checkpoints
 cp -r $SRC_DIR/examples $PREFIX/share/datashader-examples
+rm -rf $PREFIX/share/datashader-examples/.ipynb_checkpoints
 
 popd
 
