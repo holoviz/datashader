9b0d0ddf44a6e5662cfb5e03c5daca78d4bc2ea0
gbrener <gbrener@continuum.io>
2017-11-14 08:35:40 -0600

    Tweak fill rule

--- datashader/glyphs.py
+++ datashader/glyphs.py
@@ -415,17 +415,13 @@ def _build_extend_triangles(draw_triangle, map_onto_pixel):
 
             # Prevent double-drawing edges.
             # https://msdn.microsoft.com/en-us/library/windows/desktop/bb147314(v=vs.85).aspx
-            # Always draw edges of the last triangle.
-            if n < (n_tris-1):
-                bias0, bias1, bias2 = -1, -1, -1
-                if by > ay or bx < ax:
-                    bias0 = 0
-                if cy > by or cx < bx:
-                    bias1 = 0
-                if ay > cy or ax < cx:
-                    bias2 = 0
-            else:
-                bias0, bias1, bias2 = 0, 0, 0
+            bias0, bias1, bias2 = -1, -1, -1
+            if by > ay or bx < ax:
+                bias0 = 0
+            if cy > by or cx < bx:
+                bias1 = 0
+            if ay > cy or ax < cx:
+                bias2 = 0
 
             # Get bounding box
             minx = min(ax, bx, cx)
