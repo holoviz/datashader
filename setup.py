aada73b964045c26387658c680316e43ee288606
C. E. Ball <ceball@gmail.com>
2018-02-18 12:56:04 +0000

    WIP: automatic conda package.

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
+import os
 from setuptools import find_packages, setup
 
-with open('requirements.txt') as f:
-    install_requires = f.read().strip().split('\n')
+meta = dict(
+    name='datashader',
+    version=os.environ.get("VERSIONHACK","0.6.5"),
+    description='Data visualization toolchain based on aggregating into a grid',
+    url='http://datashader.org',
+    python_requires=">=2.7",
+    install_requires=[
+        'dask >=0.15.4',
+        'datashape >=0.5.1',
+        'numba >=0.35.0',
+        'numpy >=1.7',
+        'pandas >=0.20.3',
+        'pillow >=3.1.1',
+        'xarray >=0.9.6',
+        'toolz >=0.7.4',
+        'colorcet >=0.9.0',
+        'param >=1.5.0,<2.0',
+    ],
+    tests_require=[
+        'pytest >=2.8.5',
+        'pytest-benchmark >=3.0.0',
+        'rasterio',
+        'scipy',
+    ],
+    license='New BSD',
+    packages=find_packages(),
+    include_package_data=True
+)
 
-setup(name='datashader',
-      version='0.6.5',
-      description='Data visualization toolchain based on aggregating into a grid',
-      url='http://github.com/bokeh/datashader',
-      install_requires=install_requires,
-      packages=find_packages(),
-      zip_safe=False,
-      include_package_data=True)
+# TODO: decide later what to do about this (share on win?)
+# Copy all examples files but exclude IPython checkpoints
+#cp -r $SRC_DIR/examples $PREFIX/share/datashader-examples
+#rm -rf $PREFIX/share/datashader-examples/.ipynb_checkpoints
+
+if __name__ == '__main__':
+    setup(**meta)
