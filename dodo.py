f705af2fd3d0092f972dc0deefc0f1019e46903c
C. E. Ball <ceball@gmail.com>
2018-02-19 20:27:30 +0000

    Impractical to run examples on CI systems.

--- dodo.py
+++ dodo.py
@@ -1,5 +1,7 @@
 from pyct import *
 
+# TODO: running the notebooks (nb_tests) not included in all_tests
+# because it's too expensive for CI systems.
 def task_all_tests():
-    return {'actions': [],
-            'task_dep': ['nb_tests','nb_lint','unit_tests','lint']}
+    return {'actions': None,
+            'task_dep': ['nb_lint','unit_tests','lint']}
