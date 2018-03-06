f59fb032df8fc2c124aa177a3bf20a6249749013
C. E. Ball <ceball@gmail.com>
2018-03-06 18:07:44 +0000

    Use autover (from param). Express dependencies in setup.py. Two conda recipes (base and examples).

--- setup.py
+++ setup.py
@@ -1,9 +1,53 @@
 import os
 from setuptools import find_packages, setup
 
+def embed_version(basepath, reponame, ref='v0.2.1'):
+    """
+    Autover is purely a build time dependency in all cases (conda and
+    pip) except for when you use pip's remote git support [git+url] as
+    1) you need a dynamically changing version and 2) the environment
+    starts off clean with zero dependencies installed.
+
+    This function acts as a fallback to make Version available until
+    PEP518 is commonly supported by pip to express build dependencies.
+    """
+    import io, zipfile
+    try:    from urllib.request import urlopen
+    except: from urllib import urlopen
+    response = urlopen('https://github.com/ioam/autover/archive/{ref}.zip'.format(ref=ref))
+    zf = zipfile.ZipFile(io.BytesIO(response.read()))
+    ref = ref[1:] if ref.startswith('v') else ref
+    embed_version = zf.read('autover-{ref}/autover/version.py'.format(ref=ref))
+    with open(os.path.join(basepath, reponame, 'version.py'), 'wb') as f:
+        f.write(embed_version)
+
+
+def get_setup_version(reponame):
+    """
+    Helper to get the current version from either git describe or the
+    .version file (if available).
+    """
+    basepath = os.path.split(__file__)[0]
+    version_file_path = os.path.join(basepath, reponame, '.version')
+    version = None
+    try: version = importlib.import_module(reponame + ".version") # Bundled
+    except:  # autover available as package
+        try: from autover import version
+        except:
+            try: from param import version # Try to get it from param
+            except:
+                embed_version(basepath, reponame)
+                version = importlib.import_module(reponame + ".version")
+
+    if version is not None:
+        return version.Version.setup_version(basepath, reponame, dirty='strip',
+                                             archive_commit="$Format:%h$")
+    else:
+        return json.load(open(version_file_path, 'r'))['version_string']
+
 meta = dict(
     name='datashader',
-    version=os.environ.get("VERSIONHACK","0.6.5"),
+    version=get_setup_version("datashader"),
     description='Data visualization toolchain based on aggregating into a grid',
     url='http://datashader.org',
     python_requires=">=2.7",
@@ -25,15 +69,60 @@ meta = dict(
         'rasterio',
         'scipy',
         'scikit-image', # was on travis...
-        'flake8', # was on travis
+        'flake8',
         'nbsmoke'
     ],
-#    extras_require={
-#        'examples': [
-#            # just a start
-#            'requests', # for downloading data
-#        ]
-#    }
+    extras_require={
+        # TODO: need to remove indirect dependencies unless they must be
+        # pinned
+        'examples': [
+            'attrs',
+            'beautifulsoup4',
+            'bokeh',
+            'cartopy',
+            'colorcet',
+            'graphviz',
+            'pytest',
+            'pytest-benchmark',
+            'python-graphviz',
+            'dask >=0.15.4',
+            'datashader',
+            'dill',
+            'distributed',
+            'fastparquet',
+            'geoviews',
+            'holoviews >=1.8.3',
+            'ipython', # why? notebook & ipykernel maybe?
+            'iris',
+            'jupyter',
+            'jupyter_dashboards',
+            'krb5',
+            'matplotlib',
+            'nbconvert',
+            'nbformat',
+            'networkx >=2.0',
+            'numba',
+            'numpy',
+            'pandas',
+            'paramnb',
+            'pyproj',
+            'pytables',
+            'python-snappy',
+            'rasterio',
+            'requests',
+            'scipy',
+            'shapely',
+            'snappy',
+            'statsmodels',
+            'tblib',
+            'xarray',
+            'yaml',
+# TODO: needs conda package?            
+#            'cachey',
+            'streamz ==0.2.0',
+            'webargs'
+        ]
+    },
     license='New BSD',
     packages=find_packages(),
     include_package_data=True
