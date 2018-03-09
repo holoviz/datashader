151bc111e09d0a8916591ed7c757eb7a1ecd7370
C. E. Ball <ceball@gmail.com>
2018-03-09 01:03:18 +0000

    Start with unit tests.

--- dodo.py
+++ dodo.py
@@ -4,4 +4,4 @@ from pyct import *
 # because it's too expensive for CI systems.
 def task_all_tests():
     return {'actions': None,
-            'task_dep': ['nb_lint','unit_tests','lint']}
+            'task_dep': ['unit_tests','lint','nb_lint']}
