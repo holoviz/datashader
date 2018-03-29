01d7f5af0fcdde5d0362f80461a118c9ca3ca660
C. E. Ball <ceball@gmail.com>
2018-03-29 13:28:27 +0100

    Reorganization to clean up packaging of examples. And running of tests. Intermediate commit.

--- examples/conftest.py
+++ examples/conftest.py
@@ -1,14 +1,14 @@
 import pytest
 
 quick = set([
-    'examples/getting_started/2_Pipeline.ipynb',
-#    'examples/user_guide/3_Timeseries.ipynb'
+    'datashader/examples/getting_started/2_Pipeline.ipynb',
+#    'datashader/examples/user_guide/3_Timeseries.ipynb'
 ])
 
 # ipynb in examples are "example_notebook"; they can additionally be "quick" 
 def pytest_collection_modifyitems(items):
     for item in items:
-        if item.nodeid.split("::")[0].startswith("examples"):
+        if item.nodeid.split("::")[0].startswith("datashader/examples"):
             item.add_marker(pytest.mark.example_notebook)
         # NOTE: not sure where I ought to get name from - is this the
         # right bit of pytest api, and will it work on windows?
