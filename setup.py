ee235130d3750361899203197bc96655180a9a20
C. E. Ball <ceball@gmail.com>
2018-04-13 12:25:40 +0100

    Updated autover setup.py fns.

--- setup.py
+++ setup.py
@@ -6,55 +6,47 @@ from setuptools import find_packages, setup
 
 ########## autover ##########
 
-# TODO: do we have to copy/paste into all projects?
-
-def embed_version(basepath, ref='v0.2.1'):
+def embed_version(basepath, ref='v0.2.2'):
     """
     Autover is purely a build time dependency in all cases (conda and
     pip) except for when you use pip's remote git support [git+url] as
     1) you need a dynamically changing version and 2) the environment
     starts off clean with zero dependencies installed.
-
     This function acts as a fallback to make Version available until
     PEP518 is commonly supported by pip to express build dependencies.
     """
-    import io, zipfile
+    import io, zipfile, importlib
     try:    from urllib.request import urlopen
     except: from urllib import urlopen
-    response = urlopen('https://github.com/ioam/autover/archive/{ref}.zip'.format(ref=ref))
-    zf = zipfile.ZipFile(io.BytesIO(response.read()))
-    ref = ref[1:] if ref.startswith('v') else ref
-    embed_version = zf.read('autover-{ref}/autover/version.py'.format(ref=ref))
-    with open(os.path.join(basepath, 'version.py'), 'wb') as f:
-        f.write(embed_version)
-
+    try:
+        url = 'https://github.com/ioam/autover/archive/{ref}.zip'
+        response = urlopen(url.format(ref=ref))
+        zf = zipfile.ZipFile(io.BytesIO(response.read()))
+        ref = ref[1:] if ref.startswith('v') else ref
+        embed_version = zf.read('autover-{ref}/autover/version.py'.format(ref=ref))
+        with open(os.path.join(basepath, 'version.py'), 'wb') as f:
+            f.write(embed_version)
+        return importlib.import_module("version")
+    except:
+        return None
 
 def get_setup_version(reponame):
     """
     Helper to get the current version from either git describe or the
     .version file (if available).
     """
+    import json, importlib
     basepath = os.path.split(__file__)[0]
     version_file_path = os.path.join(basepath, reponame, '.version')
-    version = None
-    try: version = importlib.import_module(reponame + ".version") # Bundled
-    except:  # autover available as package
-        try: from autover import version
-        except:
-            try: from param import version # Try to get it from param
-            except:
-                embed_version(basepath)
-                version = importlib.import_module("version")
-
-    # TODO: try/except below until new param release
     try:
-        if version is not None:
-            return version.Version.setup_version(basepath, reponame, archive_commit="$Format:%h$")
-        else:
-            print("WARNING: autover unavailable. If you are installing a package, this warning can safely be ignored. If you are creating a package or otherwise operating in a git repository, you should refer to autover's documentation to bundle autover or add it as a dependency.")
-            return json.load(open(version_file_path, 'r'))['version_string']
+        from param import version
     except:
-        return '0.0.0+unknown'
+        version = embed_version(basepath)
+    if version is not None:
+        return version.Version.setup_version(basepath, reponame, archive_commit="$Format:%h$")
+    else:
+        print("WARNING: param>=1.6.0 unavailable. If you are installing a package, this warning can safely be ignored. If you are creating a package or otherwise operating in a git repository, you should install param>=1.6.0.")
+        return json.load(open(version_file_path, 'r'))['version_string']
 
 
 
