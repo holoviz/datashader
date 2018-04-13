0c6407bb01425795d3342acb7a4c15f607ab605f
Chris Ball <ceball@gmail.com>
2018-04-14 00:49:51 +0100

    Fixed path checking for windows and when examples copied elsewhere.

--- examples/conftest.py
+++ examples/conftest.py
@@ -1,16 +1,15 @@
+import os
 import pytest
 
 quick = set([
-    'datashader/examples/getting_started/2_Pipeline.ipynb',
-#    'datashader/examples/user_guide/3_Timeseries.ipynb'
+    'getting_started/2_Pipeline.ipynb',
+#    'user_guide/3_Timeseries.ipynb'
 ])
 
 # ipynb in examples are "example_notebook"; they can additionally be "quick" 
 def pytest_collection_modifyitems(items):
     for item in items:
-        if item.nodeid.split("::")[0].startswith("datashader/examples"):
+        if os.path.splitext(item.fspath)[1].lower() == ".ipynb":
             item.add_marker(pytest.mark.example_notebook)
-        # NOTE: not sure where I ought to get name from - is this the
-        # right bit of pytest api, and will it work on windows?
-        if item.nodeid.split("::")[0] in quick:
+        if os.path.relpath(str(item.fspath),os.path.commonpath([os.path.dirname(__file__),str(item.fspath)])).replace(os.path.sep,"/") in quick:
             item.add_marker(pytest.mark.quick)
