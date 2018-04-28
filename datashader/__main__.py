cf4d07e4c23186311f812946b6256deddd72614c
Chris B <ceball@gmail.com>
2018-04-28 11:39:57 +0100

    Use more of pvutil since no other commands.

--- datashader/__main__.py
+++ datashader/__main__.py
@@ -1,25 +1,12 @@
-import argparse
-
-from . import __version__
-
-pvcommands = ['install_examples',
-              'download_data']
-
-try:
-    from pvutil.cmd import add_pv_commands
-except ImportError:
-    def add_pv_commands(parser,*args,**kw):
-        from . import _missing_cmd
-        for c in pvcommands:
-            p = parser.add_parser(c); p.set_defaults(func=lambda args: p.error(_missing_cmd()))
-
 def main(args=None):
-    parser = argparse.ArgumentParser(description="datashader commands")
-    parser.add_argument('--version', action='version', version='%(prog)s '+__version__)
-    subparsers = parser.add_subparsers(title='available commands')
-    add_pv_commands(subparsers,pvcommands,'datashader',args)
-    args = parser.parse_args()
-    args.func(args) if hasattr(args,'func') else parser.error("must supply command to run") 
+    try:
+        import pvutil.cmd
+    except ImportError:
+        import sys
+        from . import _missing_cmd
+        print(_missing_cmd())
+        sys.exit(1)
+    return pvutil.cmd.substitute_main('datashader',args=args)
 
 if __name__ == "__main__":
     main()
