e10e7423c0ad346052532a842200487830709824
Chris B <ceball@gmail.com>
2018-05-03 00:02:21 +0100

    Copy in holoviews setup.py extensions to copy examples into package at packaging and installation time.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-import os
+import os,sys
+import shutil
+from collections import defaultdict
 from setuptools import find_packages, setup
 
 
@@ -45,6 +47,75 @@ def get_setup_version(reponame):
     else:
         print("WARNING: param>=1.6.0 unavailable. If you are installing a package, this warning can safely be ignored. If you are creating a package or otherwise operating in a git repository, you should install param>=1.6.0.")
         return json.load(open(version_file_path, 'r'))['version_string']
+
+########## examples ##########
+
+def check_pseudo_package(path):
+    """
+    Verifies that a fake subpackage path for assets (notebooks, svgs,
+    pngs etc) both exists and is populated with files.
+    """
+    if not os.path.isdir(path):
+        raise Exception("Please make sure pseudo-package %s exists." % path)
+    else:
+        assets = os.listdir(path)
+        if len(assets) == 0:
+            raise Exception("Please make sure pseudo-package %s is populated." % path)
+
+
+excludes = ['DS_Store', '.log', 'ipynb_checkpoints']
+packages = []
+extensions = defaultdict(list)
+
+def walker(top, names):
+    """
+    Walks a directory and records all packages and file extensions.
+    """
+    global packages, extensions
+    if any(exc in top for exc in excludes):
+        return
+    package = top[top.rfind('datashader'):].replace(os.path.sep, '.')
+    packages.append(package)
+    for name in names:
+        ext = '.'.join(name.split('.')[1:])
+        ext_str = '*.%s' % ext
+        if ext and ext not in excludes and ext_str not in extensions[package]:
+            extensions[package].append(ext_str)
+
+
+def examples(path='datashader-examples', verbose=False, force=False, root=__file__):
+    """
+    Copies the notebooks to the supplied path.
+    """
+    filepath = os.path.abspath(os.path.dirname(root))
+    example_dir = os.path.join(filepath, './examples')
+    if not os.path.exists(example_dir):
+        example_dir = os.path.join(filepath, '../examples')
+    if os.path.exists(path):
+        if not force:
+            print('%s directory already exists, either delete it or set the force flag' % path)
+            return
+        shutil.rmtree(path)
+    ignore = shutil.ignore_patterns('.ipynb_checkpoints', '*.pyc', '*~')
+    tree_root = os.path.abspath(example_dir)
+    if os.path.isdir(tree_root):
+        shutil.copytree(tree_root, path, ignore=ignore, symlinks=True)
+    else:
+        print('Cannot find %s' % tree_root)
+
+
+
+def package_assets(example_path):
+    """
+    Generates pseudo-packages for the examples directory.
+    """
+    examples(example_path, force=True, root=__file__)
+    for root, dirs, files in os.walk(example_path):
+        walker(root, dirs+files)
+    setup_args['packages'] += packages
+    for p, exts in extensions.items():
+        if exts:
+            setup_args['package_data'][p] = exts
 
 
 
@@ -152,7 +223,7 @@ extras_require['examples_pip'] = _examples_common
 
 ########## metadata for setuptools ##########
 
-meta = dict(
+setup_args = dict(
     name='datashader',
     version=get_setup_version("datashader"),
     description='Data visualization toolchain based on aggregating into a grid',
@@ -167,7 +238,7 @@ meta = dict(
     tests_require=extras_require['tests'],
     license='New BSD',
     packages=find_packages(),
-    include_package_data=True,
+    package_data={'geoviews': ['.version']},    
     entry_points={
         'console_scripts': [
             'datashader = datashader.__main__:main'
@@ -176,4 +247,12 @@ meta = dict(
 )
 
 if __name__ == '__main__':
-    setup(**meta)
+    example_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                'datashader/examples')
+    if 'develop' not in sys.argv:
+        package_assets(example_path)
+
+    setup(**setup_args)
+
+    if os.path.isdir(example_path):
+        shutil.rmtree(example_path)
