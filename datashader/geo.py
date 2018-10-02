28f1eca081e826ab0deac43a0356cf01c23d9a2b
brendancol <parietal.io@gmail.com>
2018-10-02 11:54:20 -0500

    added bump function

--- datashader/geo.py
+++ datashader/geo.py
@@ -7,6 +7,8 @@ from __future__ import division
 
 from random import choice
 
+from functools import partial
+
 import numpy as np
 import datashader.transfer_functions as tf
 
@@ -17,7 +19,7 @@ from xarray import DataArray
 __all__ = ['mean', 'binary', 'slope', 'aspect', 'ndvi', 'hillshade', 'generate_terrain']
 
 
-def hillshade(agg, azimuth, angle_altitude):
+def hillshade(agg, azimuth=225, angle_altitude=25):
     """Illuminates 2D DataArray from specific azimuth and altitude.
 
     Parameters
@@ -53,7 +55,7 @@ def hillshade(agg, azimuth, angle_altitude):
     altituderad = angle_altitude*np.pi/180.
     shaded = np.sin(altituderad) * np.sin(slope) + np.cos(altituderad) * np.cos(slope)*np.cos((azimuthrad - np.pi/2.) - aspect)
     data = (shaded + 1) / 2
-    return DataArray(data, attrs={'res':1})
+    return DataArray(data, attrs=agg.attrs)
 
 
 @ngjit
@@ -236,19 +238,27 @@ def _mean(data):
             out[y, x] = (a+b+c+d+e+f+g+h+i) / 9
     return out
 
-def mean(agg):
+def mean(agg, passes=1):
     """
-    Returns Mean filtered array using a 3x3 window
+    Returns Mean filtered array using a 3x3 window sum number of times
 
     Parameters
     ----------
     agg : DataArray
+    passes : int, number of times to run mean
 
     Returns
     -------
     data: DataArray
     """
-    return DataArray(_mean(agg.data), dims=['y', 'x'], attrs=agg.attrs)
+    out = None
+    for i in range(passes):
+        if out is None:
+            out = _mean(agg.data)
+        else:
+            out = _mean(out)
+
+    return DataArray(out, dims=['y', 'x'], attrs=agg.attrs)
 
 
 def generate_terrain(width, height, seed=10, iterations=10, extrusion_factor=200):
@@ -273,10 +283,25 @@ def generate_terrain(width, height, seed=10, iterations=10, extrusion_factor=200
     -------
     terrain: DataArray
     """
-    data = _gen_terrain(width, height, seed, iterations)
-    data = _add_texture(data, width, height, count=int(1e6), size=.05, mind=0.33, maxd=3)
-    data[data < .3] = 0  # create water
-    return DataArray(data * extrusion_factor, attrs={'res':1})
+
+    def _gen_heights(bumps):
+        out = np.zeros(len(bumps))
+        for i, b in enumerate(bumps):
+            x = b[0]
+            y = b[1]
+            val = agg.data[y, x]
+            if val >= .33 and val <= 3:
+                out[i] = .1
+        return out
+
+    agg = DataArray(_gen_terrain(width, height, seed, iterations),
+                    dims=['y', 'x'],
+                    attrs={'res':1})
+    bump_agg = bump(width, height, height_func=_gen_heights, spread=0)
+    agg += mean(bump_agg, passes=3)
+    agg.data[agg.data < .3] = 0  # create water
+    agg *= extrusion_factor
+    return agg
 
 def _gen_terrain(width, height, seed, iterations):
     amp_factors = list(range(10))
@@ -287,46 +312,92 @@ def _gen_terrain(width, height, seed, iterations):
     for i in reversed(range(iterations)):
         noise = _perlin(x * choice(amp_factors), y * choice(amp_factors), seed=seed + (i % 500))
         noise += _perlin(x, y, seed=seed + (i % 400)) ** 2
-
         if height_map is None:
             height_map = noise
         else:
             height_map += noise
-
     return height_map
 
-def _add_texture(data, width, height, count=100,
-                 seed=5, size=.2, mind=.33, maxd=.87):
+
+def bump(width, height, count=None, height_func=None, spread=1):
+    """
+    Generate a simple bump map
+
+    Parameters
+    ----------
+    width : int
+    height : int
+    count : int (defaults: w * h / 10)
+    seed : seed for random number generator (default: 5)
+    z : bump height (default: 1)
+    agg : referenced by within when limiting bump map
+    within : tuple boundaries
+
+    Returns
+    -------
+    bumpmap: DataArray
+
+    Note:
+    ----
+
+    This was heavily inspired by Michael McHugh's 2016 PyCon Canada talk:
+    https://www.youtube.com/watch?v=O33YV4ooHSo
+
+    Perlin noise is used to seed to terrain taken from here, but scaled from 0 - 1
+    and was written by Paul Panzer and is available here:
+    https://stackoverflow.com/questions/42147776/producing-2d-perlin-noise-with-numpy
+
+    """
+
     linx = range(width)
     liny = range(height)
-    tree_xs = np.random.choice(linx, count).tolist()
-    tree_ys = np.random.choice(liny, count).tolist()
-    trees = _finish_trees(data, list(zip(tree_xs, tree_ys)), size, mind, maxd)
-    return data + _mean(_mean(_mean(trees)))
+
+    if count is None:
+        count = width * height // 10
+
+    if height_func is None:
+        height_func = lambda bumps: np.ones(len(bumps))
+
+    bump_xs = np.random.choice(linx, count).tolist()
+    bump_ys = np.random.choice(liny, count).tolist()
+    locs = list(zip(bump_xs, bump_ys))
+    heights = height_func(locs)
+    bumps = _finish_bump(width, height, locs, heights, spread)
+    return DataArray(bumps, dims=['y', 'x'], attrs=dict(res=1))
 
 @ngjit
-def _finish_trees(data, locs, size, mind, maxd):
-    out = np.zeros_like(data)
-    for x, y in locs:
-        if data[y, x] >= mind and data[y, x] < maxd:
-            out[y, x] = size
+def _finish_bump(width, height, locs, heights, spread):
+    out = np.zeros((width, height))
+    rows, cols = out.shape
+    s = spread ** 2  # removed sqrt for perf.
+    for i in range(len(heights)):
+        x = locs[i][0]
+        y = locs[i][1]
+        z = heights[i]
+        out[y, x] = out[y, x] + z
+        if s > 0:
+            for nx in range(max(x - spread, 0), min(x + spread, width)):
+                for ny in range(max(y - spread, 0), min(y + spread, height)):
+                    d2 = (nx - x) * (nx - x) + (ny -  y) * (ny - y)
+                    if d2 <= s:
+                        out[ny, nx] = out[ny,nx] + (z * (d2 / s))
     return out
 
 
+@ngjit
+def _lerp(a, b, x):
+    return a + x * (b-a)
+
+@ngjit
+def _fade(t):
+    return 6 * t**5 - 15 * t**4 + 10 * t**3
+
+def _gradient(h,x,y):
+    vectors = np.array([[0,1],[0,-1],[1,0],[-1,0]])
+    g = vectors[h%4]
+    return g[:,:,0] * x + g[:,:,1] * y
+
 def _perlin(x, y, seed=0):
-
-    @ngjit
-    def lerp(a, b, x):
-        return a + x * (b-a)
-
-    @ngjit
-    def fade(t):
-        return 6 * t**5 - 15 * t**4 + 10 * t**3
-
-    def gradient(h,x,y):
-        vectors = np.array([[0,1],[0,-1],[1,0],[-1,0]])
-        g = vectors[h%4]
-        return g[:,:,0] * x + g[:,:,1] * y
 
     # permutation table
     np.random.seed(seed)
@@ -343,18 +414,17 @@ def _perlin(x, y, seed=0):
     yf = y - yi
 
     # fade factors
-    u = fade(xf)
-    v = fade(yf)
+    u = _fade(xf)
+    v = _fade(yf)
 
     # noise components
-    n00 = gradient(p[p[xi]+yi], xf, yf)
-    n01 = gradient(p[p[xi]+yi+1], xf, yf-1)
-    n11 = gradient(p[p[xi+1]+yi+1], xf-1, yf-1)
-    n10 = gradient(p[p[xi+1]+yi], xf-1, yf)
+    n00 = _gradient(p[p[xi]+yi], xf, yf)
+    n01 = _gradient(p[p[xi]+yi+1], xf, yf-1)
+    n11 = _gradient(p[p[xi+1]+yi+1], xf-1, yf-1)
+    n10 = _gradient(p[p[xi+1]+yi], xf-1, yf)
 
     # combine noises
-    x1 = lerp(n00, n10, u)
-    x2 = lerp(n01, n11, u)
-    a = lerp(x1, x2, v)
+    x1 = _lerp(n00, n10, u)
+    x2 = _lerp(n01, n11, u)
+    a = _lerp(x1, x2, v)
     return a
-
