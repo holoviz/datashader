4f7ae2988cfd879ed4ba8adab2812a863e1d30fc
Jon Mease <jon.mease@gmail.com>
2019-01-05 12:07:40 -0500

    Add failing test case for bug 570
    With jit disabled, this combination of x_range, y_range, width, height, and line
    coordinates results in an index out of bounds exception.  With jit enabled,
    this results in a non-deterministic segmentation fault.

--- datashader/tests/test_pandas.py
+++ datashader/tests/test_pandas.py
@@ -533,3 +533,23 @@ def test_trimesh_agg_api():
     cvs = ds.Canvas(x_range=(0, 10), y_range=(0, 10))
     agg = cvs.trimesh(pts, tris, agg=ds.mean('weight'))
     assert agg.shape == (600, 600)
+
+
+def test_bug_570():
+    # See https://github.com/pyviz/datashader/issues/570
+    df = pd.DataFrame({
+        'Time': [1456353642.2053893, 1456353642.2917893],
+        'data': [-59.4948743433377, 506.4847376716022],
+    }, columns=['Time', 'data'])
+
+    x_range = (1456323293.9859753, 1456374687.0009754)
+    y_range = (-228.56721300380943, 460.4042291124646)
+
+    cvs = ds.Canvas(x_range=x_range, y_range=y_range,
+                    plot_height=300, plot_width=1000)
+    agg = cvs.line(df, 'Time', 'data', agg=ds.count())
+
+    # Check location of line
+    yi, xi = np.where(agg.values == 1)
+    assert np.array_equal(yi, np.arange(73, 300))
+    assert np.array_equal(xi, np.array([590] * len(yi)))
