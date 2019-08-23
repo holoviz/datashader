2fb495a019c0823b5ea8cb1be5c881f020d52992
brendancol <bcollins@parietal.io>
2019-08-23 13:57:54 -0400

    wip: initial commit of zonal function

--- datashader/spatial/__init__.py
+++ datashader/spatial/__init__.py
@@ -1,2 +1,3 @@
 from __future__ import absolute_import
 from datashader.spatial.points import read_parquet  # noqa (API import)
+from datashader.spatial.zonal import zonal_stats  # noqa (API import)
