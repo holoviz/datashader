b257c8d82e3712817266c0a2e62180bbd3a7ff4c
Brendan Collins <bcollins@parietal.io>
2019-08-30 12:28:05 -0500

    Proximity spatial function and docs (#772)

--- datashader/spatial/__init__.py
+++ datashader/spatial/__init__.py
@@ -1,2 +1,7 @@
 from __future__ import absolute_import
 from datashader.spatial.points import read_parquet  # noqa (API import)
+
+from datashader.spatial.proximity import proximity  # noqa (API import)
+from datashader.spatial.proximity import great_circle_distance  # noqa (API import)
+from datashader.spatial.proximity import euclidean_distance  # noqa (API import)
+from datashader.spatial.proximity import manhattan_distance  # noqa (API import)
