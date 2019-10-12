bfe769a4485492b76ef5b5e5d84325b25e99892d
brendancol <bcollins@parietal.io>
2019-10-12 10:48:03 -0500

    improved bump mapping performance by elimiating .tolist() calls when selecting target locations

--- datashader/geo.py
+++ datashader/geo.py
@@ -433,10 +433,12 @@ def bump(width, height, count=None, height_func=None, spread=1):
     if height_func is None:
         height_func = lambda bumps: np.ones(len(bumps))
 
-    bump_xs = np.random.choice(linx, count).tolist()
-    bump_ys = np.random.choice(liny, count).tolist()
-    locs = tuple(zip(bump_xs, bump_ys))
+    locs = np.empty((count, 2), dtype=np.uint16)
+    locs[:, 0] = np.random.choice(linx, count)
+    locs[:, 1] = np.random.choice(liny, count)
+
     heights = height_func(locs)
+
     bumps = _finish_bump(width, height, locs, heights, spread)
     return DataArray(bumps, dims=['y', 'x'], attrs=dict(res=1))
 
