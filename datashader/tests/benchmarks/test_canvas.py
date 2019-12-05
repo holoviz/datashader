329ae8986e5db0ba6c8d1365848b064778e8bc49
Jon Mease <jon.mease@gmail.com>
2019-12-04 21:35:23 -0500

    GPU testing improvements (#841)
    * Add DATASHADER_TEST_GPU environment variable to force GPU tests
    * Add GPU benchmarking cases to run if DATASHADER_TEST_GPU is set

--- datashader/tests/benchmarks/test_canvas.py
+++ datashader/tests/benchmarks/test_canvas.py
@@ -1,14 +1,19 @@
 import pytest
-
+import os
 import numpy as np
 import pandas as pd
 
 import datashader as ds
 
+if "DATASHADER_TEST_GPU" in os.environ:
+    test_gpu = bool(int(os.environ["DATASHADER_TEST_GPU"]))
+else:
+    test_gpu = None
+
 
 @pytest.fixture
 def time_series():
-    n = 10**6
+    n = 10**7
     signal = np.random.normal(0, 0.3, size=n).cumsum() + 50
     noise = lambda var, bias, n: np.random.normal(bias, var, n)
     ys = signal + noise(1, 10*(np.random.random() - 0.5), n)
@@ -26,5 +31,23 @@ def test_line(benchmark, time_series):
 
 @pytest.mark.benchmark(group="canvas")
 def test_points(benchmark, time_series):
+    cvs = ds.Canvas(plot_height=300, plot_width=900)
+    benchmark(cvs.points, time_series, 'x', 'y')
+
+
+@pytest.mark.skipif(test_gpu is not True, reason="DATASHADER_TEST_GPU not set")
+@pytest.mark.benchmark(group="canvas")
+def test_line_gpu(benchmark, time_series):
+    from cudf import from_pandas
+    time_series = from_pandas(time_series)
+    cvs = ds.Canvas(plot_height=300, plot_width=900)
+    benchmark(cvs.line, time_series, 'x', 'y')
+
+
+@pytest.mark.skipif(test_gpu is not True, reason="DATASHADER_TEST_GPU not set")
+@pytest.mark.benchmark(group="canvas")
+def test_points_gpu(benchmark, time_series):
+    from cudf import from_pandas
+    time_series = from_pandas(time_series)
     cvs = ds.Canvas(plot_height=300, plot_width=900)
     benchmark(cvs.points, time_series, 'x', 'y')
