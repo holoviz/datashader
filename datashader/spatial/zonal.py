20f7de156567be51e54a5a1871799ccbf0790713
brendancol <bcollins@parietal.io>
2019-12-31 12:34:27 -0500

    added crosstab, apply functions to spatial.zonal module

--- datashader/spatial/zonal.py
+++ datashader/spatial/zonal.py
@@ -1,8 +1,18 @@
 import numpy as np
 import pandas as pd
+import warnings
+warnings.simplefilter('default')
 
 
-def zonal_stats(zones, values, stats=['mean', 'max', 'min', 'std', 'var']):
+def zonal_stats(zones, values,
+                stat_funcs=['mean', 'max', 'min', 'std', 'var']):
+    warnings.warn('\'zonal_stats\' is deprecated. Use \'stats\' instead',
+                  DeprecationWarning)
+
+    return stats(zones, values, stat_funcs)
+
+
+def stats(zones, values, stat_funcs=['mean', 'max', 'min', 'std', 'var']):
     """Calculate statistics for each zone defined by a zone dataset, based on
     values from another dataset (value raster).
 
@@ -31,9 +41,33 @@ def zonal_stats(zones, values, stats=['mean', 'max', 'min', 'std', 'var']):
 
     Returns
     -------
-    zonal_stats_df: pandas.DataFrame
+    stats_df: pandas.DataFrame
         A pandas DataFrame where each column is a statistic
         and each row is a zone with zone id.
+
+    Examples
+    --------
+    >>> zones_val = np.array([[1, 1, 0, 2],
+    >>>                      [0, 2, 1, 2]])
+    >>> zones = xarray.DataArray(zones_val)
+    >>> values_val = np.array([[2, -1, 5, 3],
+    >>>                       [3, np.nan, 20, 10]])
+    >>> values = xarray.DataArray(values_val)
+    
+    # default setting
+    >>> df = stats(zones, values)
+    >>> df
+        mean	max 	min 	std     	var
+    1	7.0 	20.0	-1.0	9.273618	86.00
+    2	6.5	    10.0   	3.0	    3.500000	12.25
+
+    # custom stat
+    >>> custom_stats ={'sum': lambda val: val.sum()}
+    >>> df = stats(zones, values)
+    >>> df
+        sum
+    1	21.0
+    2	13.0
 
     """
 
@@ -48,11 +82,11 @@ def zonal_stats(zones, values, stats=['mean', 'max', 'min', 'std', 'var']):
         "`zones.values` and `values.values` must have same shape"
 
     assert issubclass(type(zones_val[0, 0]), np.integer),\
-        "`zones.values` must be an array of integer"
+        "`zones.values` must be an array of integers"
 
     assert issubclass(type(values_val[0, 0]), np.integer) or\
            issubclass(type(values_val[0, 0]), np.float),\
-        "`values.values` must be an array of integer or float"
+        "`values.values` must be an array of integers or floats"
 
     unique_zones = np.unique(zones_val).astype(int)
 
@@ -64,10 +98,8 @@ def zonal_stats(zones, values, stats=['mean', 'max', 'min', 'std', 'var']):
     # mask out all invalid values_val such as: nan, inf
     masked_values = np.ma.masked_invalid(values_val)
 
-    if isinstance(stats, dict):
-
-        cols = stats.keys()
-        zonal_stats_df = pd.DataFrame(columns=list(cols))
+    if isinstance(stat_funcs, dict):
+        stats_df = pd.DataFrame(columns=[*stat_funcs])
 
         for zone_id in unique_zones:
             # do not consider 0 pixels as a zone
@@ -79,16 +111,16 @@ def zonal_stats(zones, values, stats=['mean', 'max', 'min', 'std', 'var']):
                                              masked_values)
 
             zone_stats = []
-            for stat in stats:
-                stat_func = stats.get(stat)
+            for stat in stat_funcs:
+                stat_func = stat_funcs.get(stat)
                 if not callable(stat_func):
                     raise ValueError(stat)
                 zone_stats.append(stat_func(zone_values))
 
-            zonal_stats_df.loc[zone_id] = zone_stats
+            stats_df.loc[zone_id] = zone_stats
 
     else:
-        zonal_stats_df = pd.DataFrame(columns=stats)
+        stats_df = pd.DataFrame(columns=stat_funcs)
 
         for zone_id in unique_zones:
             # do not consider 0 pixels as a zone
@@ -100,7 +132,7 @@ def zonal_stats(zones, values, stats=['mean', 'max', 'min', 'std', 'var']):
                                              masked_values)
 
             zone_stats = []
-            for stat in stats:
+            for stat in stat_funcs:
                 if stat == 'mean':
                     zone_stats.append(zone_values.mean())
                 elif stat == 'max':
@@ -112,14 +144,172 @@ def zonal_stats(zones, values, stats=['mean', 'max', 'min', 'std', 'var']):
                 elif stat == 'var':
                     zone_stats.append(zone_values.var())
                 else:
-                    err_str = 'In function zonal_stats(). '\
+                    err_str = 'In function stats(). ' \
                               + '\'' + stat + '\' option not supported.'
                     raise ValueError(err_str)
 
-            zonal_stats_df.loc[zone_id] = zone_stats
+            stats_df.loc[zone_id] = zone_stats
 
-    num_df_rows = len(zonal_stats_df.index)
-    assert num_df_rows == num_zones,\
+    num_df_rows = len(stats_df.index)
+    assert num_df_rows == num_zones, \
         'Output dataframe must have same number of rows as of zones.values'
 
-    return zonal_stats_df
+    return stats_df
+
+
+def crosstab(zones, values):
+    """Calculate cross-tabulated areas between two datasets: a zone dataset,
+    a value dataset (a value raster). Outputs a pandas DataFrame.
+
+    Parameters
+    ----------
+    zones: xarray.DataArray,
+        zones.values is a 2d array of integers.
+        A zone is all the cells in a raster that have the same value,
+        whether or not they are contiguous. The input zone layer defines
+        the shape, values, and locations of the zones. An integer field
+        in the zone input is specified to define the zones.
+
+    values: xarray.DataArray,
+        values.values is a 2d array of integers or floats.
+        The input value raster contains the input values used in calculating
+        the categorical statistic for each zone.
+
+    Returns
+    -------
+    crosstab_df: pandas.DataFrame
+        A pandas DataFrame where each column is a pixel value
+        and each row is a zone with zone id.
+
+    Examples
+    --------
+    >>> zones_val = np.array([[1, 1, 0, 2],
+    >>>                      [0, 2, 1, 2]])
+    >>> zones = xarray.DataArray(zones_val)
+    >>> values_val = np.array([[2, -1, 5, 3],
+    >>>                       [3, np.nan, 20, 10]])
+    >>> values = xarray.DataArray(values_val)
+    >>> crosstab_df = crosstab(zones, values)
+    >>> crosstab_df
+          -1      2 	  3       5       10      20
+    1     1       1       0       0       0       1
+    2     0       0       1       0       1       0
+    """
+
+    # return of the function
+    crosstab_df = pd.DataFrame()
+
+    zones_val = zones.values
+    values_val = values.values
+
+    assert zones_val.shape == values_val.shape, \
+        "`zones.values` and `values.values` must have same shape"
+
+    assert issubclass(type(zones_val[0, 0]), np.integer), \
+        "`zones.values` must be an array of integers"
+
+    assert issubclass(type(values_val[0, 0]), np.integer) or \
+           issubclass(type(values_val[0, 0]), np.float), \
+        "`values.values` must be an array of integers or floats"
+
+    unique_zones = np.unique(zones_val).astype(int)
+    num_zones = len(unique_zones)
+    # do not consider zone with 0s
+    if 0 in unique_zones:
+        num_zones = len(unique_zones) - 1
+
+    if num_zones == 0:
+        warnings.warn("No zone in `zones` xarray.")
+
+    # mask out all invalid values_val such as: nan, inf
+    masked_values = np.ma.masked_invalid(values_val)
+    unique_masked = np.unique(masked_values)
+
+    # get unique pixel values (exclude invalid value: nan, inf)
+    unique_values = unique_masked[unique_masked.mask == False].data
+
+    if len(unique_values) == 0:
+        warnings.warn("No value in `values` xarray.")
+        return crosstab_df
+
+    # columns are pixel values
+    crosstab_df = pd.DataFrame(columns=unique_values)
+
+    for zone_id in unique_zones:
+        # do not consider entries in `zones` with id=0 as a zone
+        if zone_id == 0:
+            continue
+
+        # get zone values_val
+        zone_values = np.ma.masked_where(zones_val != zone_id, masked_values)
+
+        zone_stats = []
+        for val in unique_values:
+            # count number of `val` pixels in `zone_values`
+            count = np.ma.masked_where(zone_values != val, zone_values).count()
+            zone_stats.append(count)
+
+        crosstab_df.loc[zone_id] = zone_stats
+
+    num_df_rows = len(crosstab_df.index)
+    assert num_df_rows == num_zones, \
+        'Output dataframe must have same number of rows as of zones.values'
+
+    return crosstab_df
+
+
+def apply(zones, agg, func, zone_id):
+    """Apply a function to a zone with zone_id. Change the agg content.
+
+    Parameters
+    ----------
+    zones: xarray.DataArray,
+        zones.values is a 2d array of integers.
+        A zone is all the cells in a raster that have the same value,
+        whether or not they are contiguous. The input zone layer defines
+        the shape, values, and locations of the zones. An integer field
+        in the zone input is specified to define the zones.
+
+    agg: xarray.DataArray,
+        agg.values is a 2d array of integers or floats.
+        The input value raster.
+
+    func: callable function to apply to the zone with zone_id
+
+    zone_id: integer
+
+    Returns
+    -------
+
+    Examples
+    --------
+    >>> zones_val = np.array([[1, 1, 0, 2],
+    >>>                      [0, 2, 1, 2]])
+    >>> zones = xarray.DataArray(zones_val)
+    >>> values_val = np.array([[1, 1, 1, 1],
+    >>>                       [1, np.nan, 1, 1]])
+    >>> agg = xarray.DataArray(values_val)
+    >>> func = lambda x: 0
+    >>> zone_id = 2
+    >>> apply(zones, agg, func, zone_id)
+    >>> agg
+    >>> array([[1, 1, 1, 0],
+    >>>        [1, 0, 1, 0]])
+    """
+
+    assert zones.values.shape == agg.values.shape, \
+        "`zones.values` and `values.values` must have same shape"
+
+    assert issubclass(type(zones.values[0, 0]), np.integer), \
+        "`zones.values` must be an array of integers"
+
+    assert issubclass(type(agg.values[0, 0]), np.integer) or \
+           issubclass(type(agg.values[0, 0]), np.float), \
+        "`agg.values` must be an array of integers or floats"
+
+    # boolean array to indicate if an entry is in the zone
+    zone = zones.values == zone_id
+    # apply func to the agg corresponding to the zone
+    agg.values[zone] = func(agg.values[zone])
+    return
+
--- datashader/tests/test_zonal.py
+++ datashader/tests/test_zonal.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import xarray as xa
 
-from datashader.spatial import zonal_stats
+from datashader.spatial import stats, crosstab, apply
 
 
 zones_val = np.array([[0, 1, 1, 2, 4, 0, 0],
@@ -16,7 +16,6 @@ values_val = np.array([[0, 12, 10, 2, 3.25, np.nan, np.nan],
                        [np.nan, 3.5, -9, 4, 2, 0, np.inf]])
 values = xa.DataArray(values_val)
 
-num_zones = 3
 unique_values = [1, 2, 4]
 
 masked_values = np.ma.masked_invalid(values.values)
@@ -32,10 +31,10 @@ zone_stds = [zone_vals_1.std(), zone_vals_2.std(), zone_vals_3.std()]
 zone_vars = [zone_vals_1.var(), zone_vals_2.var(), zone_vals_3.var()]
 
 
-@pytest.mark.zonal_stats
-def test_zonal_stats_default():
-    # default stats=['mean', 'max', 'min', 'std', 'var']
-    df = zonal_stats(zones=zones, values=values)
+@pytest.mark.stats
+def test_stats_default():
+    # default stat_funcs=['mean', 'max', 'min', 'std', 'var']
+    df = stats(zones=zones, values=values)
 
     assert isinstance(df, pd.DataFrame)
 
@@ -54,8 +53,8 @@ def test_zonal_stats_default():
     assert zone_vars == df['var'].tolist()
 
 
-@pytest.mark.zonal_stats
-def test_zonal_stats_custom_stat():
+@pytest.mark.stats
+def test_stats_custom_stat():
     cal_sum = lambda values: values.sum()
 
     def cal_double_sum(values):
@@ -68,8 +67,8 @@ def test_zonal_stats_custom_stat():
                         cal_double_sum(zone_vals_2),
                         cal_double_sum(zone_vals_3)]
 
-    stats = {'sum': cal_sum, 'double sum': cal_double_sum}
-    df = zonal_stats(zones=zones, values=values, stats=stats)
+    custom_stats ={'sum': cal_sum, 'double sum': cal_double_sum}
+    df = stats(zones=zones, values=values, stat_funcs=custom_stats)
 
     assert isinstance(df, pd.DataFrame)
 
@@ -85,51 +84,187 @@ def test_zonal_stats_custom_stat():
     assert zone_double_sums == df['double sum'].tolist()
 
 
-@pytest.mark.zonal_stats
-def test_zonal_stats_invalid_custom_stat():
+@pytest.mark.stats
+def test_stats_invalid_custom_stat():
 
     cal_sum = lambda values, zones: values + zones
-    stats = {'sum': cal_sum}
+    custom_stats ={'sum': cal_sum}
 
     # custom stat only takes 1 argument. Thus, raise error
     with pytest.raises(Exception) as e_info:
-        zonal_stats(zones=zones, values=values, stats=stats)
-        assert e_info
+        df = stats(zones=zones, values=values, stat_funcs=custom_stats)
 
 
-@pytest.mark.zonal_stats
-def test_zonal_stats_invalid_stat_list():
-    stats = ['some_stat']
+@pytest.mark.stats
+def test_stats_invalid_stat_list():
+    custom_stats = ['some_stat']
     with pytest.raises(Exception) as e_info:
-        zonal_stats(zones=zones, values=values, stats=stats)
-        assert e_info
+        df = stats(zones=zones, values=values, stat_funcs=custom_stats)
 
 
-@pytest.mark.zonal_stats
-def test_zonal_stats_invalid_zones():
-    zones = np.array([1, 2, 0.5])
-    values = np.array([1, 2, 0.5])
+@pytest.mark.stats
+def test_stats_invalid_zones():
+    zones = xa.DataArray(np.array([1, 2, 0.5]))
+    values = xa.DataArray(np.array([1, 2, 0.5]))
 
     with pytest.raises(Exception) as e_info:
-        zonal_stats(zones=zones, values=values)
-        assert e_info
+        df = stats(zones=zones, values=values)
 
 
-@pytest.mark.zonal_stats
-def test_zonal_stats_invalid_values():
-    zones = np.array([1, 2, 0], dtype=np.int)
-    values = np.array(['apples', 'foobar', 'cowboy'])
-
-    with pytest.raises(Exception) as e_info:
-        zonal_stats(zones=zones, values=values)
-        assert e_info
-
-
-@pytest.mark.zonal_stats
-def test_zonal_stats_mismatch_zones_values_shape():
-    zones = np.array([1, 2, 0])
-    values = np.array([1, 2, 0, np.nan])
+@pytest.mark.stats
+def test_stats_invalid_values():
+    zones = xa.DataArray(np.array([1, 2, 0], dtype=np.int))
+    values = xa.DataArray(np.array(['apples', 'foobar', 'cowboy']))
 
     with pytest.raises(Exception) as e_info:
-        zonal_stats(zones=zones, values=values)
-        assert e_info
+        df = stats(zones=zones, values=values)
+
+
+@pytest.mark.stats
+def test_stats_mismatch_zones_values_shape():
+    zones = xa.DataArray(np.array([1, 2, 0]))
+    values = xa.DataArray(np.array([1, 2, 0, np.nan]))
+
+    with pytest.raises(Exception) as e_info:
+        df = stats(zones=zones, values=values)
+
+
+# test crosstab
+@pytest.mark.crosstab
+def test_crosstab_invalid_zones():
+    zones = xa.DataArray(np.array([1, 2, 0.5]))
+    values = xa.DataArray(np.array([1, 2, 0.5]))
+
+    with pytest.raises(Exception) as e_info:
+        df = crosstab(zones=zones, values=values)
+
+
+@pytest.mark.crosstab
+def test_crosstab_invalid_values():
+    zones = xa.DataArray(np.array([1, 2, 0], dtype=np.int))
+    values = xa.DataArray(np.array(['apples', 'foobar', 'cowboy']))
+
+    with pytest.raises(Exception) as e_info:
+        df = crosstab(zones=zones, values=values)
+
+
+@pytest.mark.crosstab
+def test_crosstab_mismatch_zones_values_shape():
+    zones = xa.DataArray(np.array([1, 2, 0]))
+    values = xa.DataArray(np.array([1, 2, 0, np.nan]))
+
+    with pytest.raises(Exception) as e_info:
+        df = crosstab(zones=zones, values=values)
+
+
+# test case 1: no zones
+@pytest.mark.crosstab
+def test_crosstab_no_zones():
+    outline = np.array([[0, 0, 0],
+                        [0, 0, 0],
+                        [0, 0, 0]])
+    outline_zones = xa.DataArray(outline)
+
+    outline_val = np.array([[0, 12, 10],
+                            [-2.5, np.nan, 7],
+                            [np.nan, 3.5, np.inf]])
+    # there are 6 values in total" -2.5, 0, 3.5, 7, 10, 12
+    num_values = 6
+
+    values = xa.DataArray(outline_val)
+
+    df = crosstab(zones=outline_zones, values=values)
+
+    assert len(df.columns) == num_values
+
+
+# test case 2: no values
+@pytest.mark.crosstab
+def test_crosstab_no_zones():
+    zones_val = np.array([[0, 1, 3],
+                          [0, 1, 4],
+                          [0, 0, 2]])
+    zones = xa.DataArray(zones_val)
+
+    values_val = np.array([[np.nan, np.nan, np.nan],
+                           [np.nan, np.nan, np.nan],
+                           [np.nan, np.inf, -np.inf]])
+    values = xa.DataArray(values_val)
+
+    df = crosstab(zones=zones, values=values)
+    assert len(df) == 0
+
+
+# test case 3: each zone only has a value
+@pytest.mark.crosstab
+def test_crosstab():
+    zones_val = np.array([[0, 1, 2],
+                          [3, 4, 5],
+                          [6, 7, 8]])
+    zones = xa.DataArray(zones_val)
+
+    values_val = np.array([[0, 1, 2],
+                           [3, 4, 5],
+                           [6, 7, 8]])
+    values = xa.DataArray(values_val)
+
+    df = crosstab(zones=zones, values=values)
+
+    num_rows = len(df.index)
+    num_cols = len(df.columns)
+
+    for i in range(num_cols):
+        for j in range(1, num_rows + 1):
+            if i == j:
+                assert df[i][j] == 1
+            else:
+                assert df[i][j] == 0
+
+
+# test apply
+@pytest.mark.apply
+def test_apply_invalid_zones():
+    zones = xa.DataArray(np.array([1, 2, 0.5]))
+    agg = xa.DataArray(np.array([1, 2, 0.5]))
+
+    with pytest.raises(Exception) as e_info:
+        df = apply(zones=zones, agg=agg)
+
+
+@pytest.mark.apply
+def test_apply_invalid_agg():
+    zones = xa.DataArray(np.array([1, 2, 0], dtype=np.int))
+    agg = xa.DataArray(np.array(['apples', 'foobar', 'cowboy']))
+
+    with pytest.raises(Exception) as e_info:
+        df = apply(zones=zones, agg=agg)
+
+
+@pytest.mark.apply
+def test_apply_mismatch_zones_agg_shape():
+    zones = xa.DataArray(np.array([1, 2, 0]))
+    agg = xa.DataArray(np.array([1, 2, 0, np.nan]))
+
+    with pytest.raises(Exception) as e_info:
+        df = apply(zones=zones, agg=agg)
+
+
+@pytest.mark.apply
+def test_apply():
+    zones_val = np.array([[0, 1, 2],
+                          [3, 4, 5],
+                          [6, 7, 8]])
+    zones = xa.DataArray(zones_val)
+
+    agg_val = np.array([[0, 1, 2],
+                        [3, 4, 5],
+                        [6, 7, 8]])
+    agg = xa.DataArray(agg_val)
+    agg_copy = agg.copy()
+
+    func = lambda x: 0
+    zone_idx = np.unique(zones_val)
+    for zone_id in zone_idx:
+        apply(zones, agg, func, zone_id)
+        # agg.shape remains the same
+        assert agg.shape == agg_copy.shape
