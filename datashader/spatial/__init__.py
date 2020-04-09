ca58b692474895628b4424e9e68c732a3351d77b
James A. Bednar <jbednar@users.noreply.github.com>
2020-04-09 09:47:48 -0500

    Update datashader/spatial/__init__.py

--- datashader/spatial/__init__.py
+++ datashader/spatial/__init__.py
@@ -12,7 +12,7 @@ from datashader.spatial.proximity import manhattan_distance  # noqa (API import)
 from datashader.spatial.viewshed import viewshed  # noqa (API import)
 from datashader.spatial.zonal import zonal_stats  # noqa (API import)
 
-from ..utils import VisisbleDeprecationWarning
+from ..utils import VisibleDeprecationWarning
 
 warnings.warn(
     "The datashader.spatial module is deprecated as of version 0.11.0. "
