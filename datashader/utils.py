6d4dc0f5b7fdba78cd564aaaf20becd537fdd74e
Philipp Rudiger <prudiger@anaconda.com>
2020-04-25 03:38:27 +0200

    Deprecated geo and spatial modules (#894)

--- datashader/utils.py
+++ datashader/utils.py
@@ -30,6 +30,15 @@ except ImportError:
     GeometryDtype = type(None)
 
 
+class VisibleDeprecationWarning(UserWarning):
+    """Visible deprecation warning.
+
+    By default, python will not show deprecation warnings, so this class
+    can be used when a very visible warning is helpful, for example because
+    the usage is most likely a user bug.
+    """
+
+
 ngjit = nb.jit(nopython=True, nogil=True)
 ngjit_parallel = nb.jit(nopython=True, nogil=True, parallel=True)
 
