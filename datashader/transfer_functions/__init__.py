9a2639e6ad609cb90538ad4004a51fb5da17dacd
Philipp Rudiger <prudiger@anaconda.com>
2020-05-02 23:40:17 +0200

    Removed old masked_clip_2d

--- datashader/transfer_functions/__init__.py
+++ datashader/transfer_functions/__init__.py
@@ -190,39 +190,6 @@ def _normalize_interpolate_how(how):
     raise ValueError("Unknown interpolation method: {0}".format(how))
 
 
-@ngjit
-def masked_clip_2d(data, mask, lower, upper):
-    """
-    Clip the elements of an input array between lower and upper bounds,
-    skipping over elements that are masked out.
-
-    Parameters
-    ----------
-    data: np.ndarray
-        Numeric ndarray that will be clipped in-place
-    mask: np.ndarray
-        Boolean ndarray where True values indicate elements that should be
-        skipped
-    lower: int or float
-        Lower bound to clip to
-    upper: int or float
-        Upper bound to clip to
-
-    Returns
-    -------
-    None
-        data array is modified in-place
-    """
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            if mask[i, j]:
-                continue
-            val = data[i, j]
-            if val < lower:
-                data[i, j] = lower
-            elif val > upper:
-                data[i, j] = upper
-
 def _interpolate(agg, cmap, how, alpha, span, min_alpha, name):
     if cupy and isinstance(agg.data, cupy.ndarray):
         from ._cuda_utils import masked_clip_2d, interp
