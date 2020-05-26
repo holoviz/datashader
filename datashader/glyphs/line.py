fcd3337175ecfaca36d3ae34f87c1189f45b8b57
Valentin Haenel <vhaenel@anaconda.com>
2020-05-26 13:19:06 +0200

    enable code reuse

--- datashader/glyphs/line.py
+++ datashader/glyphs/line.py
@@ -669,9 +669,7 @@ def _xiaolinwu(i, x0, x1, y0, y1, append, *aggs_and_cols):
         x, y = px
         if not y < agg.shape[0] or not x < agg.shape[1]:
             return
-        c = cols.copy()
-        c[i] *= value
-        append(i, x, y, agg, c)
+        _unsafe_draw_pixel(py, c)
 
     def _flipxy(px, py):
         if steep:
