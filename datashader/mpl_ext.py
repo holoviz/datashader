cda454aebaf73c9e9e272229b223fa86063ef198
Nezar Abdennur <nabdennur@gmail.com>
2020-08-11 11:51:24 -0400

    Add categorical legend element maker

--- datashader/mpl_ext.py
+++ datashader/mpl_ext.py
@@ -1,7 +1,9 @@
+from matplotlib.image import _ImageBase
+from matplotlib.patches import Patch
 from matplotlib.transforms import Bbox, TransformedBbox, BboxTransform
 import matplotlib as mpl
-import matplotlib.image as mimage
 import numpy as np
+import xarray as xr
 
 from . import transfer_functions as tf
 from . import Canvas
@@ -13,7 +15,7 @@ def uint32_to_uint8(img):
     return img.view(dtype=np.uint8).reshape(img.shape + (4,))
 
 
-class DSArtist(mimage._ImageBase):
+class DSArtist(_ImageBase):
     def __init__(
         self,
         ax,
@@ -42,10 +44,70 @@ class DSArtist(mimage._ImageBase):
         self.axes = ax
         self.set_array([[1, 1], [1, 1]])
 
+    def _aggregate(self, x_range, y_range):
+        dims = self.axes.patch.get_window_extent().bounds
+        plot_width = int(dims[3] + 0.5)
+        plot_height = int(dims[2] + 0.5)
+
+        canvas = Canvas(
+            plot_width=int(plot_width * self.pipeline.width_scale),
+            plot_height=int(plot_height * self.pipeline.height_scale),
+            x_range=x_range,
+            y_range=y_range,
+        )
+        binned = bypixel(
+            self.pipeline.df, canvas, self.pipeline.glyph, self.pipeline.agg
+        )
+        binned = self.pipeline.transform_fn(binned)
+
+        return binned
+
+    def _infer_color_dict(self, binned):
+        # Infer the color key by sampling each categorical channel independently.
+        name = binned.dims[2]
+        categories = binned.coords[name].data
+        n_categories = len(categories)
+
+        # Make a row of one-hot vectors for each category (1, n, n).
+        # tf.shade generates a warning if there are singleton dims, so we pad
+        # with an extra row (2, n, n).
+        onehot = np.expand_dims(np.eye(n_categories), 0)
+        onehot = np.concatenate([onehot, onehot], axis=0)
+
+        # Convert to xarray so tf.shade knows what to do.
+        onehot = xr.DataArray(
+            onehot, dims=binned.dims
+        ).reindex({name: categories})
+
+        # Extract categorical colors from the raster.
+        raster = self.pipeline.color_fn(onehot)
+        rgb = uint32_to_uint8(raster.data)[0]
+        color_key = [mpl.colors.to_hex(c) for c in rgb / 255.0]
+
+        return dict(zip(categories, color_key))
+
+    def _infer_colormap(self, binned):
+        # Infer the color map by passing a linear ramp of values through the
+        # color_fn.
+        vmin = binned.min()
+        vmax = binned.max()
+        norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+        vramp = np.linspace(vmin, vmax, 256)
+
+        # Convert to a 2D xarray to tf.shade can use it.
+        vramp = xr.DataArray(vramp[:, np.newaxis], dims=binned.dims)
+        cramp = self.pipeline.color_fn(vramp)
+
+        # Extract the colors from the raster.
+        colors = uint32_to_uint8(cramp.data.ravel())
+        cmap = mpl.colors.ListedColormap(colors / 255.0, name="from_datashader")
+
+        return norm, cmap
+
     def make_image(self, renderer, magnification=1.0, unsampled=False):
-        trans = self.get_transform()
-        (x1, x2), (y1, y2) = self.axes.get_xlim(), self.axes.get_ylim()
+        x1, x2, y1, y2 = self.get_extent()
         bbox = Bbox(np.array([[x1, y1], [x2, y2]]))
+        trans = self.get_transform()
         transformed_bbox = TransformedBbox(bbox, trans)
 
         dims = self.axes.patch.get_window_extent().bounds
@@ -53,52 +115,26 @@ class DSArtist(mimage._ImageBase):
         plot_height = int(dims[2] + 0.5)
 
         # binning part of pipeline
-        canvas = Canvas(
-            plot_width=int(plot_width * self.pipeline.width_scale),
-            plot_height=int(plot_height * self.pipeline.height_scale),
-            x_range=(x1, x2),
-            y_range=(y1, y2),
-        )
-        binned = bypixel(
-            self.pipeline.df, canvas, self.pipeline.glyph, self.pipeline.agg
-        )
-        binned = self.pipeline.transform_fn(binned)
-
-        # save the binned data for cursor events
-        self._ds_data = binned.data
-
-        # infer the colormap or legend depending on aggregation
-        if len(bins.shape) == 3:
-            cdata = np.eye(bins.shape[-1])[:, np.newaxis]
-            agg_name, agg_index = list(bins.indexes.items())[-1]
-            cpalette = self.pipeline.color_fn(
-                tf.Image(cdata, dims=bins.dims).reindex({agg_name: agg_index})
+        binned = self._aggregate([x1, x2], [y1, y2])
+        if binned.ndim == 2:
+            norm, cmap = self._infer_colormap(binned)
+            self.set_norm(norm)
+            self.set_cmap(cmap)
+        elif binned.ndim != 3:
+            raise ValueError(
+                "Aggregated DataArray must have 2 or 3 dimensions; "
+                "got array with shape {}".format(binned.shape)
             )
-            colors = uint32_to_uint8(cpalette.data.ravel())
-            colors[:, -1] = 255  # set alpha to max
-            for lab, col in zip(agg_index.values, colors):
-                # TODO: remove colormap and use categorical legend
-                pass
-
-        else:
-            vmin = bins.min()
-            vmax = bins.max()
-            vramp = np.linspace(vmin, vmax, 256)
-            cramp = da.pipeline.color_fn(tf.Image(vramp[:, np.newaxis])).data.ravel()
-            colors = uint32_to_uint8(cramp)
-            cmap = mpl.colors.ListedColormap(colors / 256, name="from_datashader")
-            norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
-            da.set_norm(norm)
-            da.set_cmap(cmap)
-
         # shading part of pipeline
-        img = self.pipeline.color_fn(binned)
-        img = self.pipeline.spread_fn(img)
+        raster = self.pipeline.color_fn(binned)
+        raster = self.pipeline.spread_fn(raster)
 
+        # save the binned data DataArray for cursor events
         # save the uint32 image DataArray for inspection
-        self._ds_image = img
+        self._ds_data = binned
+        self._ds_image = raster
 
-        rgba = uint32_to_uint8(img.data)
+        rgba = uint32_to_uint8(raster.data)
         rgba = np.ma.masked_array(rgba)
         # self.set_clim(vmin, vmax)
         self.set_array(rgba)
@@ -112,6 +148,17 @@ class DSArtist(mimage._ImageBase):
             unsampled=unsampled,
         )
 
+    def get_legend_elements(self):
+        x_range, y_range = self.axes.get_xlim(), self.axes.get_ylim()
+        binned = self._aggregate(x_range, y_range)
+        if binned.ndim != 3:
+            return None
+        color_dict = self._infer_color_dict(binned)
+        return [
+            Patch(facecolor=color, edgecolor='none', label=name)
+            for name, color in color_dict.items()
+        ]
+
     def get_extent(self):
         (x1, x2), (y1, y2) = self.axes.get_xlim(), self.axes.get_ylim()
         return x1, x2, y1, y2
@@ -122,7 +169,7 @@ class DSArtist(mimage._ImageBase):
         if self.origin == "upper":
             ymin, ymax = ymax, ymin
 
-        arr = self._ds_data
+        arr = self._ds_data.data
         data_extent = Bbox([[ymin, xmin], [ymax, xmax]])
         array_extent = Bbox([[0, 0], arr.shape[:2]])
         trans = BboxTransform(boxin=data_extent, boxout=array_extent)
