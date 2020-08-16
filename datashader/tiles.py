73148c371b4bd7994bb83115db18da3853f20294
Philipp Rudiger <prudiger@anaconda.com>
2020-08-16 16:15:41 +0200

    Handle boolean span in tile generation code (#949)

--- datashader/tiles.py
+++ datashader/tiles.py
@@ -39,13 +39,21 @@ def calculate_zoom_level_stats(super_tiles, load_data_func,
                                color_ranging_strategy='fullscan'):
     if color_ranging_strategy == 'fullscan':
         stats = []
+        is_bool = False
         for super_tile in super_tiles:
             agg = _get_super_tile_min_max(super_tile, load_data_func, rasterize_func)
             super_tile['agg'] = agg
-            stats.append(np.nanmin(agg.data))
-            stats.append(np.nanmax(agg.data))
-        b = db.from_sequence(stats)
-        return super_tiles, dask.compute(b.min(), b.max())
+            if agg.dtype.kind == 'b':
+                is_bool = True
+            else:
+                stats.append(np.nanmin(agg.data))
+                stats.append(np.nanmax(agg.data))
+        if is_bool:
+            span = (0, 1)
+        else:
+            b = db.from_sequence(stats)
+            span = dask.compute(b.min(), b.max())
+        return super_tiles, span
     else:
         raise ValueError('Invalid color_ranging_strategy option')
 
