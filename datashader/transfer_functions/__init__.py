7f60a83601ca3cd8e5b647eb79e3ac6d4b0c4639
jlstevens <jlrstevens@gmail.com>
2020-08-19 17:55:27 +0200

    Removed dstack step from inner loop

--- datashader/transfer_functions/__init__.py
+++ datashader/transfer_functions/__init__.py
@@ -615,7 +615,7 @@ def spread(img, px=1, shape='circle', how=None, mask=None, name=None):
                 buf = np.zeros(padded_shape, dtype=layer.dtype)
             kernel(layer.data, mask, buf)
             layers.append(buf[extra:-extra, extra:-extra].copy())
-            out = np.dstack(layers)
+        out = np.dstack(layers)
 
     return img.__class__(out, dims=img.dims, coords=img.coords, name=name)
 
