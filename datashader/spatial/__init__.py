c9ff5d38d7401f1e24e6878e42b2a76a021caaf9
thuydotm <thuydtm.csk9@gmail.com>
2020-08-25 19:06:25 -0400

    remove viewshed

--- datashader/spatial/__init__.py
+++ datashader/spatial/__init__.py
@@ -9,7 +9,6 @@ from datashader.spatial.proximity import great_circle_distance  # noqa (API impo
 from datashader.spatial.proximity import euclidean_distance  # noqa (API import)
 from datashader.spatial.proximity import manhattan_distance  # noqa (API import)
 
-from datashader.spatial.viewshed import viewshed  # noqa (API import)
 from datashader.spatial.zonal import zonal_stats  # noqa (API import)
 
 from ..utils import VisibleDeprecationWarning
