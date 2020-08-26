3a983cb0c767dd60e5f898b0725011d9b69e1d14
thuydotm <thuydtm.csk9@gmail.com>
2020-08-26 15:18:00 -0400

    ds.spatial: remove proximity and zonal

--- datashader/spatial/__init__.py
+++ datashader/spatial/__init__.py
@@ -4,13 +4,6 @@ import warnings
 
 from datashader.spatial.points import read_parquet  # noqa (API import)
 
-from datashader.spatial.proximity import proximity  # noqa (API import)
-from datashader.spatial.proximity import great_circle_distance  # noqa (API import)
-from datashader.spatial.proximity import euclidean_distance  # noqa (API import)
-from datashader.spatial.proximity import manhattan_distance  # noqa (API import)
-
-from datashader.spatial.zonal import zonal_stats  # noqa (API import)
-
 from ..utils import VisibleDeprecationWarning
 
 warnings.warn(
