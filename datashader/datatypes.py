d20b606bb58e4bfcad2f678c891f43188833c4ee
James A. Bednar <jbednar@users.noreply.github.com>
2021-09-29 11:37:45 -0500

    Update datashader/datatypes.py

--- datashader/datatypes.py
+++ datashader/datatypes.py
@@ -585,7 +585,7 @@ Invalid indices for take with allow_fill True: {inds}""".format(
         else:
             if len(self) == 0 and len(indices) > 0:
                 raise IndexError(
-                    "cannot do a non-empty take from an empty axes"
+                    "Cannot do a non-empty take from an empty axis"
                 )
 
             sequence = [self[i] for i in indices]
