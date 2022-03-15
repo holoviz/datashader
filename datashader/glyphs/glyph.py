holoviz/datashader

commit 7fa1b340ddbc26e37d452796b197401713cb695d

Fix/cudf api updates (#1044)
* replace with updated cudf APIs

* update cudf minimum version to latest stable

* replace cudf as_gpu_matrix with to_cupy

* more efficient way of converting cudf series to cupy array

* add conditional usage based on cudf version installed

* revert cudf min version dependency

* use packaging.version.Version

datashader/glyphs/glyph.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division
+from packaging.version import Version
 import inspect
 import warnings
 import os
@@ -91,13 +92,13 @@ class Glyph(Expr):
         return minval, maxval
 
     @staticmethod
-    def to_gpu_matrix(df, columns):
-        if not isinstance(columns, (list, tuple)):
-            return df[columns].to_gpu_array()
+    def to_cupy_array(df, columns):
+        if cudf.__version__ >= Version("22.02"):
+            return df[columns].to_cupy()
         else:
-            return cudf.concat([
-                df[name].rename(str(i)) for i, name in enumerate(columns)
-            ], axis=1).as_gpu_matrix()
+            if not isinstance(columns, (list, tuple)):
+                return df[columns].to_cupy_array()
+            return df[columns].as_gpu_matrix()
 
     def expand_aggs_and_cols(self, append):
         """

datashader/glyphs/points.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division
+from packaging.version import Version
 import numpy as np
 from toolz import memoize
 
@@ -17,7 +18,11 @@ except Exception:
 
 def values(s):
     if isinstance(s, cudf.Series):
-        return s.to_gpu_array(fillna=np.nan)
+        if cudf.__version__ >= Version("22.02"):
+            return s.to_cupy(na_value=np.nan)
+        else:
+            return s.to_gpu_array(fillna=np.nan)
+        
     else:
         return s.values
 
@@ -191,8 +196,8 @@ class Point(_PointLike):
             xmin, xmax, ymin, ymax = bounds
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = df[x_name].to_gpu_array(fillna=np.nan)
-                ys = df[y_name].to_gpu_array(fillna=np.nan)
+                xs = values(df[x_name])
+                ys = values(df[y_name])
                 do_extend = extend_cuda[cuda_args(xs.shape[0])]
             else:
                 xs = df[x_name].values

datashader/reductions.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, division, print_function
-
+from packaging.version import Version
 import numpy as np
 from datashape import dshape, isnumeric, Record, Option
 from datashape import coretypes as ct
@@ -42,6 +42,8 @@ class extract(Preprocess):
                 nullval = np.nan
             else:
                 nullval = 0
+            if cudf.__version__ >= Version("22.02"):
+                return df[self.column].to_cupy(na_value=nullval)
             return cupy.array(df[self.column].to_gpu_array(fillna=nullval))
         elif isinstance(df, xr.Dataset):
             # DataArray could be backed by numpy or cupy array
@@ -81,6 +83,8 @@ class category_codes(CategoryPreprocess):
 
     def apply(self, df):
         if cudf and isinstance(df, cudf.DataFrame):
+            if cudf.__version__ >= Version("22.02"):
+                return df[self.column].cat.codes.to_cupy()
             return df[self.column].cat.codes.to_gpu_array()
         else:
             return df[self.column].cat.codes.values
@@ -113,7 +117,9 @@ class category_modulo(category_codes):
 
     def apply(self, df):
         result = (df[self.column] - self.offset) % self.modulo
-        if cudf and isinstance(df, cudf.DataFrame):
+        if cudf and isinstance(df, cudf.Series):
+            if cudf.__version__ >= Version("22.02"):
+                return result.to_cupy()
             return result.to_gpu_array()
         else:
             return result.values
@@ -193,7 +199,10 @@ class category_values(CategoryPreprocess):
             else:
                 nullval = 0
             a = cupy.asarray(a)
-            b = cupy.asarray(df[self.column].to_gpu_array(fillna=nullval))
+            if cudf.__version__ >= Version("22.02"):
+                b = df[self.column].to_cupy(na_value=nullval)
+            else:
+                b = cupy.asarray(df[self.column].fillna(nullval))
             return cupy.stack((a, b), axis=-1)
         else:
             b = df[self.column].values

datashader/glyphs/area.py
@@ -115,8 +115,8 @@ class AreaToZeroAxis0(_PointLike):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = self.to_gpu_matrix(df, x_name)
-                ys = self.to_gpu_matrix(df, y_name)
+                xs = self.to_cupy_array(df, x_name)
+                ys = self.to_cupy_array(df, y_name)
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[x_name].values
@@ -199,9 +199,9 @@ class AreaToLineAxis0(_AreaToLineLike):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = self.to_gpu_matrix(df, x_name)
-                ys0 = self.to_gpu_matrix(df, y_name)
-                ys1 = self.to_gpu_matrix(df, y_stack_name)
+                xs = self.to_cupy_array(df, x_name)
+                ys0 = self.to_cupy_array(df, y_name)
+                ys1 = self.to_cupy_array(df, y_stack_name)
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[x_name].values
@@ -291,8 +291,8 @@ class AreaToZeroAxis0Multi(_PointLike):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = self.to_gpu_matrix(df, x_names)
-                ys = self.to_gpu_matrix(df, y_names)
+                xs = self.to_cupy_array(df, x_names)
+                ys = self.to_cupy_array(df, y_names)
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[list(x_names)].values
@@ -382,9 +382,9 @@ class AreaToLineAxis0Multi(_AreaToLineLike):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = self.to_gpu_matrix(df, x_names)
-                ys0 = self.to_gpu_matrix(df, y_names)
-                ys1 = self.to_gpu_matrix(df, y_stack_names)
+                xs = self.to_cupy_array(df, x_names)
+                ys0 = self.to_cupy_array(df, y_names)
+                ys1 = self.to_cupy_array(df, y_stack_names)
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[list(x_names)].values
@@ -490,8 +490,8 @@ class AreaToZeroAxis1(_PointLike):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = df[list(x_names)].as_gpu_matrix()
-                ys = df[list(y_names)].as_gpu_matrix()
+                xs = self.to_cupy_array(df,list(x_names))
+                ys = self.to_cupy_array(df,list(y_names))
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[list(x_names)].values
@@ -597,9 +597,9 @@ class AreaToLineAxis1(_AreaToLineLike):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = df[list(x_names)].as_gpu_matrix()
-                ys = df[list(y_names)].as_gpu_matrix()
-                y_stacks = df[list(y_stack_names)].as_gpu_matrix()
+                xs = self.to_cupy_array(df,list(x_names))
+                ys = self.to_cupy_array(df,list(y_names))
+                y_stacks = self.to_cupy_array(df,list(y_stack_names))
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[list(x_names)].values
@@ -666,7 +666,7 @@ class AreaToZeroAxis1XConstant(AreaToZeroAxis1):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                ys = df[list(y_names)].as_gpu_matrix()
+                ys = self.to_cupy_array(df,list(y_names))
                 do_extend = extend_cuda[cuda_args(ys.shape)]
             else:
                 ys = df[list(y_names)].values
@@ -746,8 +746,8 @@ class AreaToLineAxis1XConstant(AreaToLineAxis1):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                ys = df[list(y_names)].as_gpu_matrix()
-                y_stacks = df[list(y_stack_names)].as_gpu_matrix()
+                ys = self.to_cupy_array(df,list(y_names))
+                y_stacks = self.to_cupy_array(df,list(y_stack_names))
                 do_extend = extend_cuda[cuda_args(ys.shape)]
             else:
                 ys = df[list(y_names)].values
@@ -814,7 +814,7 @@ class AreaToZeroAxis1YConstant(AreaToZeroAxis1):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = df[list(x_names)].as_gpu_matrix()
+                xs = self.to_cupy_array(df,list(x_names))
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[list(x_names)].values
@@ -880,7 +880,7 @@ class AreaToLineAxis1YConstant(AreaToLineAxis1):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = df[list(x_names)].as_gpu_matrix()
+                xs = self.to_cupy_array(df,list(x_names))
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[list(x_names)].values

datashader/glyphs/line.py
@@ -59,8 +59,8 @@ class LineAxis0(_PointLike, _AntiAliasedLine):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = self.to_gpu_matrix(df, x_name)
-                ys = self.to_gpu_matrix(df, y_name)
+                xs = self.to_cupy_array(df, x_name)
+                ys = self.to_cupy_array(df, y_name)
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[x_name].values
@@ -145,8 +145,8 @@ class LineAxis0Multi(_PointLike, _AntiAliasedLine):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = self.to_gpu_matrix(df, x_names)
-                ys = self.to_gpu_matrix(df, y_names)
+                xs = self.to_cupy_array(df, x_names)
+                ys = self.to_cupy_array(df, y_names)
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[list(x_names)].values
@@ -254,8 +254,8 @@ class LinesAxis1(_PointLike, _AntiAliasedLine):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = self.to_gpu_matrix(df, x_names)
-                ys = self.to_gpu_matrix(df, y_names)
+                xs = self.to_cupy_array(df, x_names)
+                ys = self.to_cupy_array(df, y_names)
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[list(x_names)].values
@@ -324,7 +324,7 @@ class LinesAxis1XConstant(LinesAxis1):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                ys = self.to_gpu_matrix(df, y_names)
+                ys = self.to_cupy_array(df, y_names)
                 do_extend = extend_cuda[cuda_args(ys.shape)]
             else:
                 ys = df[list(y_names)].values
@@ -394,7 +394,7 @@ class LinesAxis1YConstant(LinesAxis1):
             aggs_and_cols = aggs + info(df)
 
             if cudf and isinstance(df, cudf.DataFrame):
-                xs = self.to_gpu_matrix(df, x_names)
+                xs = self.to_cupy_array(df, x_names)
                 do_extend = extend_cuda[cuda_args(xs.shape)]
             else:
                 xs = df[list(x_names)].values

commit 15487f2dfa35503b42d57cd4c853cbaa0553749c

fix incorrect version comparison introduced in #1044

datashader/glyphs/glyph.py
@@ -93,7 +93,7 @@ class Glyph(Expr):
 
     @staticmethod
     def to_cupy_array(df, columns):
-        if cudf.__version__ >= Version("22.02"):
+        if Version(cudf.__version__) >= Version("22.02"):
             return df[columns].to_cupy()
         else:
             if not isinstance(columns, (list, tuple)):

datashader/glyphs/points.py
@@ -18,7 +18,7 @@ except Exception:
 
 def values(s):
     if isinstance(s, cudf.Series):
-        if cudf.__version__ >= Version("22.02"):
+        if Version(cudf.__version__) >= Version("22.02"):
             return s.to_cupy(na_value=np.nan)
         else:
             return s.to_gpu_array(fillna=np.nan)

datashader/reductions.py
@@ -42,7 +42,7 @@ class extract(Preprocess):
                 nullval = np.nan
             else:
                 nullval = 0
-            if cudf.__version__ >= Version("22.02"):
+            if Version(cudf.__version__) >= Version("22.02"):
                 return df[self.column].to_cupy(na_value=nullval)
             return cupy.array(df[self.column].to_gpu_array(fillna=nullval))
         elif isinstance(df, xr.Dataset):
@@ -83,7 +83,7 @@ class category_codes(CategoryPreprocess):
 
     def apply(self, df):
         if cudf and isinstance(df, cudf.DataFrame):
-            if cudf.__version__ >= Version("22.02"):
+            if Version(cudf.__version__) >= Version("22.02"):
                 return df[self.column].cat.codes.to_cupy()
             return df[self.column].cat.codes.to_gpu_array()
         else:
@@ -118,7 +118,7 @@ class category_modulo(category_codes):
     def apply(self, df):
         result = (df[self.column] - self.offset) % self.modulo
         if cudf and isinstance(df, cudf.Series):
-            if cudf.__version__ >= Version("22.02"):
+            if Version(cudf.__version__) >= Version("22.02"):
                 return result.to_cupy()
             return result.to_gpu_array()
         else:
@@ -199,7 +199,7 @@ class category_values(CategoryPreprocess):
             else:
                 nullval = 0
             a = cupy.asarray(a)
-            if cudf.__version__ >= Version("22.02"):
+            if Version(cudf.__version__) >= Version("22.02"):
                 b = df[self.column].to_cupy(na_value=nullval)
             else:
                 b = cupy.asarray(df[self.column].fillna(nullval))