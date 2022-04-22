a324c23413775fbb9bfb4dd08139da990fc4e0ed
Ian Thomas <ianthomas23@gmail.com>
2022-04-22 13:26:44 +0100

    Fix benchmark tests

--- datashader/tests/benchmarks/test_draw_line.py
+++ datashader/tests/benchmarks/test_draw_line.py
@@ -39,7 +39,8 @@ def test_draw_line_left_border(benchmark, draw_line):
     x1, y1 = (0, n)
 
     agg = np.zeros((n+1, n+1), dtype='i4')
-    benchmark(draw_line, sx, tx, sy, ty, xmin, xmax, ymin, ymax, x0, y0, x1, y1, 0, True, agg)
+    benchmark(draw_line, 0, sx, tx, sy, ty, xmin, xmax, ymin, ymax, True, True,
+              x0, x1, y0, y1, 0.0, 0.0, agg)
 
 
 @py2_skip
@@ -50,7 +51,9 @@ def test_draw_line_diagonal(benchmark, draw_line):
     x1, y1 = (n, n)
 
     agg = np.zeros((n+1, n+1), dtype='i4')
-    benchmark(draw_line, sx, tx, sy, ty, xmin, xmax, ymin, ymax, x0, y0, x1, y1, 0, True, agg)
+    benchmark(draw_line, 0, sx, tx, sy, ty, xmin, xmax, ymin, ymax, True, True,
+              x0, x1, y0, y1, 0.0, 0.0, agg)
+
 
 @py2_skip
 @pytest.mark.benchmark(group="draw_line")
@@ -60,4 +63,5 @@ def test_draw_line_offset(benchmark, draw_line):
     x1, y1 = (n, n//4-1)
 
     agg = np.zeros((n+1, n+1), dtype='i4')
-    benchmark(draw_line, sx, tx, sy, ty, xmin, xmax, ymin, ymax, x0, y0, x1, y1, 0, True, agg)
+    benchmark(draw_line, 0, sx, tx, sy, ty, xmin, xmax, ymin, ymax, True, True,
+              x0, x1, y0, y1, 0.0, 0.0, agg)
