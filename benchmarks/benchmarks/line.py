4b80d52527a289946b9bda3a3084fa83a284b47e
Ian Thomas <ianthomas23@gmail.com>
2022-10-21 09:43:45 +0100

    Add cudf, dask and dask-cudf Canvas.line benchmarks (#1140)
    * Add cudf, dask and dask-cudf line benchmarks
    
    * Linting

--- benchmarks/benchmarks/line.py
+++ benchmarks/benchmarks/line.py
@@ -1,13 +1,28 @@
+import dask.dataframe as dd
 import datashader as ds
 import numpy as np
 import pandas as pd
+from .common import DataLibrary
+
+try:
+    import cudf
+except:
+    cudf = None
+
+try:
+    import dask_cudf
+except:
+    dask_cudf = None
 
 
 class Line:
-    param_names = ("line_count", "line_width", "self_intersect")
-    params = ([1000, 10000], [0, 1, 2], [False, True])
+    param_names = ("data_library", "line_count", "line_width", "self_intersect")
+    params = (
+        [DataLibrary.PandasDF, DataLibrary.DaskDF, DataLibrary.CuDF, DataLibrary.DaskCuDF],
+        [1000, 10000], [0, 1, 2], [False, True],
+    )
 
-    def setup(self, line_count, line_width, self_intersect):
+    def setup(self, data_library, line_count, line_width, self_intersect):
         canvas_size = 1000
         points_per_line = 10
 
@@ -21,9 +36,29 @@ class Line:
         )
         self.df = pd.DataFrame(y)
 
-    def time_LinesAxis1XConstant(self, line_count, line_width, self_intersect):
-        if line_width == 0.0 and not self_intersect:
+        if data_library == DataLibrary.PandasDF:
+            pass
+        elif data_library == DataLibrary.DaskDF:
+            self.df = dd.from_pandas(self.df, npartitions=4)
+        elif data_library == DataLibrary.CuDF:
+            if cudf:
+                self.df = cudf.DataFrame.from_pandas(self.df)
+            else:
+                raise NotImplementedError("CuDF not available")
+        elif data_library == DataLibrary.DaskCuDF:
+            if dask_cudf:
+                cdf = cudf.DataFrame.from_pandas(self.df)
+                self.df = dask_cudf.from_cudf(cdf, npartitions=4)
+            else:
+                raise NotImplementedError("Dask-cuDF not available")
+        else:
+            raise NotImplementedError(f"data_library {data_library} not supported in this test")
+
+    def time_LinesAxis1XConstant(self, data_library, line_count, line_width, self_intersect):
+        if line_width == 0 and not self_intersect:
             raise NotImplementedError  # Same as line_width=0, self_intersect=False
+        elif line_width > 0 and data_library not in [DataLibrary.PandasDF, DataLibrary.DaskDF]:
+            raise NotImplementedError  # Antialiased lines only work on CPU not GPU
 
         agg = ds.count(self_intersect=self_intersect)
         self.canvas.line(
