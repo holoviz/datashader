963b7eb1bf776aaa4a452142fd8b7e4d96d3eade
Jonatan Aponte <japonted025@gmail.com>
2023-02-21 17:47:06 +0100

    1141 | Validate canvas width, height (#1183)
    * 1141 | Validate canvas width, height
    
    * Add tests for canvas extend
    
    * Rename to validate size and fix spacing
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Jonatan Aponte <jonatan-armando.aponte-duenas@xarvio.com>
    Co-authored-by: Jonatan Aponte <jonatanaponte@Jonatans-MBP.fritz.box>
    Co-authored-by: Ian Thomas <ianthomas23@gmail.com>

--- datashader/tests/test_dask.py
+++ datashader/tests/test_dask.py
@@ -1564,3 +1564,21 @@ def test_line_antialias_where(npartitions):
             sol_where_min[j, i] = agg_where_min[j, i] = nan
 
     assert_eq_ndarray(agg_where_min.data, sol_where_min)
+
+
+def test_canvas_size():
+    cvs_list = [
+        ds.Canvas(plot_width=0, plot_height=6),
+        ds.Canvas(plot_width=5, plot_height=0),
+        ds.Canvas(plot_width=0, plot_height=0),
+        ds.Canvas(plot_width=-1, plot_height=1),
+        ds.Canvas(plot_width=10, plot_height=-1)
+    ]
+    msg = r'Invalid size: plot_width and plot_height must be bigger than 0'
+    df = pd.DataFrame(dict(x=[0, 0.2, 1], y=[0, 0.4, 1], z=[10, 20, 30]))
+    ddf = dd.from_pandas(df, 1)
+
+    for cvs in cvs_list:
+        with pytest.raises(ValueError, match=msg): 
+            cvs.points(ddf, "x", "y", ds.mean("z"))
+ 
