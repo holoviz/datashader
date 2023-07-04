6f66991e1479c6bcd3b594d37cdb0079bb2f432d
Ian Thomas <ianthomas23@gmail.com>
2023-07-04 16:02:48 +0100

    Add image is close test helper

--- datashader/tests/test_pandas.py
+++ datashader/tests/test_pandas.py
@@ -128,6 +128,20 @@ def assert_eq_ndarray(data, b, close=False):
         np.testing.assert_equal(data, b)
 
 
+def assert_image_close(image0, image1, tolerance):
+    def to_rgba_xr(image):
+        data = image.data
+        if cupy and isinstance(data, cupy.ndarray):
+            data = cupy.asnumpy(data)
+        shape = data.shape
+        data = data.view(np.uint8).reshape(shape + (4,))
+        return xr.DataArray(data, dims=image.dims + ("rgba",), coords=image.coords)
+
+    da0 = to_rgba_xr(image0)
+    da1 = to_rgba_xr(image1)
+    xr.testing.assert_allclose(da0, da1, atol=tolerance, rtol=0)
+
+
 def floats(n):
     """Returns contiguous list of floats from initial point"""
     while True:
