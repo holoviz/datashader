dfad43da1c65294fe1c01c6b244fa745eba30038
Ian Thomas <ianthomas23@gmail.com>
2023-09-28 17:33:01 +0100

    Remove datashape versioning

--- datashader/datashape/__init__.py
+++ datashader/datashape/__init__.py
@@ -10,7 +10,3 @@ from .discovery import discover
 from .util import *
 from .promote import promote, optionify
 from .error import DataShapeSyntaxError
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
