c493e252adc9958416f3475d6e611a5a5aae42a0
Simon Høxbro Hansen <simon.hansen@me.com>
2024-04-04 09:00:31 +0200

    Remove dask setting to conftest

--- datashader/tests/conftest.py
+++ datashader/tests/conftest.py
@@ -1,8 +1,9 @@
 import contextlib
+import dask
+
+dask.config.set(scheduler='synchronous')
 
 with contextlib.suppress(Exception):
     # From Dask 2024.3.0 they now use `dask_expr` by default
     # https://github.com/dask/dask/issues/10995
-    import dask
-
     dask.config.set({"dataframe.query-planning": False})
