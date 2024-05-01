ed73a4a6e68964110c2ecd335e3eaf3ed07ff183
Simon Høxbro Hansen <simon.hansen@me.com>
2024-05-01 18:16:34 +0200

    Clean up the download_data.py script

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,18 +1,15 @@
 from pathlib import Path
+from contextlib import suppress
 
 import pyct.cmd
 
 BASE_PATH = Path(__file__).resolve().parents[1]
 
-try:
-    import bokeh.sampledata
-except ImportError:
-    pass
-else:
-    bokeh.sampledata.download()
 
-pyct.cmd.fetch_data(
-    name="data",
-    path=str(BASE_PATH / "examples"),
-    datasets="datasets.yml",
-)
+pyct.cmd.fetch_data(name="data", path=str(BASE_PATH / "examples"), datasets="datasets.yml")
+
+
+with suppress(ImportError):
+    import bokeh.sampledata
+
+    bokeh.sampledata.download()
