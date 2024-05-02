0ee6b719c51a9a4faa9d6c04630550b6a98ccda3
Simon Høxbro Hansen <simon.hansen@me.com>
2024-05-02 07:56:41 +0200

    Clean up pyct command

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,12 +1,8 @@
-from pathlib import Path
 from contextlib import suppress
 
 import pyct.cmd
 
-BASE_PATH = Path(__file__).resolve().parents[1]
-
-
-pyct.cmd.fetch_data(name="data", path=str(BASE_PATH / "examples"), datasets="datasets.yml")
+pyct.cmd.fetch_data(name="data", path="examples", datasets="datasets.yml")
 
 
 with suppress(ImportError):
