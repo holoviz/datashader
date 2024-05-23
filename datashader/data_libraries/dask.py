ae64c6ffe48fc2ce2ff505f4064bda961239c0d4
Simon Høxbro Hansen <simon.hansen@me.com>
2024-05-23 19:41:52 +0200

    Use sys.modules

--- datashader/data_libraries/dask.py
+++ datashader/data_libraries/dask.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from contextlib import suppress
+import sys
 
 import numpy as np
 import pandas as pd
@@ -39,7 +39,7 @@ def dask_pipeline(df, schema, canvas, glyph, summary, *, antialias=False, cuda=F
 # Classic Dask.DataFrame
 bypixel.pipeline.register(dd.core.DataFrame)(dask_pipeline)
 
-with suppress(ImportError):
+if "dask_expr" in sys.modules:
     import dask_expr
 
     bypixel.pipeline.register(dask_expr.DataFrame)(dask_pipeline)
