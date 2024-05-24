77fc651a8de72b88ebdb7e803f48f8419e5ce2ab
Simon Høxbro Hansen <simon.hansen@me.com>
2024-05-24 09:44:06 +0200

    Update conftest.py

--- datashader/tests/conftest.py
+++ datashader/tests/conftest.py
@@ -25,7 +25,7 @@ def pytest_collection_modifyitems(config, items):
             skipped.append(item)
         elif empty:
             selected.append(item)
-        elif not empty and any(m in item.keywords for m in markers):
+        elif not empty and all(m in item.keywords for m in markers):
             selected.append(item)
         else:
             skipped.append(item)
