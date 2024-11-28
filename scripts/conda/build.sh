f18aa65f00e98b56133f04ded500da01ce40831f
Simon Høxbro Hansen <simon.hansen@me.com>
2024-11-28 09:31:53 +0100

    build: Small updates to build infrastructure (#1372)

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -4,13 +4,11 @@ set -euxo pipefail
 
 PACKAGE="datashader"
 
-python -m build .  # No -w, to also get examples into conda package
+python -m build --sdist .
 
 VERSION=$(python -c "import $PACKAGE; print($PACKAGE._version.__version__)")
 export VERSION
 
-# conda config --env --set conda_build.pkg_format 2
-conda build scripts/conda/recipe --no-anaconda-upload --no-verify
+conda build scripts/conda/recipe --no-anaconda-upload --no-verify -c conda-forge --package-format 1
 
-# mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.conda" dist
 mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.tar.bz2" dist
