e53509ee595099157ea6617a8254b1fc5d970134
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-01-20 18:18:19 +0100

    Update examples and other workflows

--- examples/conftest.py
+++ examples/conftest.py
@@ -1,4 +1,5 @@
 from importlib.util import find_spec
+from packaging.version import Version
 
 collect_ignore_glob = [
     "tiling.ipynb",
@@ -14,3 +15,13 @@ if find_spec("spatialpandas") is None:
         "user_guide/7_Networks.ipynb",
         "user_guide/8_Polygons.ipynb",
     ]
+
+if find_spec("dask") is not None:
+    import dask
+
+    # Spatialpandas does not support dask-expr, which is
+    # only available from this version.
+    if Version(dask.__version__).release >= (2025, 1, 0):
+        collect_ignore_glob += [
+            "user_guide/8_Polygons.ipynb",
+        ]
