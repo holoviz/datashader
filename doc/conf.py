39179f8382fa4e46965872edbf9f6c8f9761b8be
Simon Høxbro Hansen <simon.hansen@me.com>
2025-01-21 11:27:48 +0100

    docs: Fix build (#1391)
    Co-authored-by: Maxime Liquet <35924738+maximlt@users.noreply.github.com>

--- doc/conf.py
+++ doc/conf.py
@@ -15,6 +15,11 @@ html_css_files += [
     'css/custom.css'
 ]
 
+# Somehow .ipynb started to take precedence over .rst.
+# This broke the landing page `index.rst` as it embeds `index.ipynb` file.
+# Adding `.md` to make our life easier in the future.
+source_suffix = ['.rst', '.md', '.ipynb']
+
 html_logo = '_static/logo_horizontal.svg'
 html_favicon = '_static/favicon.ico'
 
@@ -87,7 +92,7 @@ def patch_error_location(self, msg, error=True):
     try:
         original_error_location(self, msg, error)
     except ValueError as e:
-        if "See Also entry ':doc:`xarray-tutorial" in str(e):
+        if "site-packages/xarray" in str(e):
             return
         else:
             raise e
