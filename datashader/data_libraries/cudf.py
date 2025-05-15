c74fb8f367c849a36b4b3cd4c89bf548e4cb58b1
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-05-15 17:57:20 +0200

    lazy_register to gpu

--- datashader/data_libraries/cudf.py
+++ datashader/data_libraries/cudf.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 from datashader.data_libraries.pandas import default
 from datashader.core import bypixel
-from .._dependencies import cudf, register_import_hook
+from .._dependencies import cudf
 
 
 def cudf_pipeline(df, schema, canvas, glyph, summary, *, antialias=False):
     return default(glyph, df, schema, canvas, summary, antialias=antialias, cuda=True)
 
 
-register_import_hook(cudf, lambda: bypixel.pipeline.register(cudf.DataFrame)(cudf_pipeline))
+bypixel.pipeline.lazy_register(cudf, "DataFrame", cudf_pipeline)
--- datashader/data_libraries/dask_cudf.py
+++ datashader/data_libraries/dask_cudf.py
@@ -1,14 +1,11 @@
 from __future__ import annotations
-from contextlib import suppress
 from datashader.data_libraries.dask import dask_pipeline
 from datashader.core import bypixel
+from .._dependencies import dask_cudf
 
 
 def dask_cudf_pipeline(df, schema, canvas, glyph, summary, *, antialias=False):
     return dask_pipeline(df, schema, canvas, glyph, summary, antialias=antialias, cuda=True)
 
 
-with suppress(ImportError):
-    import dask_cudf
-
-    dask_cudf_pipeline = bypixel.pipeline.register(dask_cudf.DataFrame)(dask_cudf_pipeline)
+bypixel.pipeline.lazy_register(dask_cudf, "DataFrame", dask_cudf_pipeline)
