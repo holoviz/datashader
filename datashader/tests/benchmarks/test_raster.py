b96bd1bcfb2f3e1b524791c3cf600e1853b9a4fe
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-09-05 12:44:47 +0200

    Add test for raster.py

---
+++ datashader/tests/benchmarks/test_raster.py
@@ -0,0 +1,29 @@
+import pytest
+import numpy as np
+import xarray as xr
+import datashader as ds
+
+from . import array_params
+
+
+@pytest.fixture(params=array_params)
+def raster_data(request):
+    size, array_module = request.param
+    data = xr.DataArray(
+        array_module.random.random((size, size)),
+        dims=["x", "y"],
+        coords={"x": np.arange(size), "y": np.arange(size)},
+        name="raster_data",
+    )
+
+    return data
+
+
+@pytest.mark.benchmark(group="raster")
+def test_raster(benchmark, raster_data):
+    def func():
+        cvs = ds.Canvas(plot_height=300, plot_width=300)
+        arr = cvs.raster(raster_data)
+        return arr.compute()
+
+    benchmark(func)
