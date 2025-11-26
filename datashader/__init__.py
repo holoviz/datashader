7b4586ecd4f8de7972b7e1946dee5573b38e49b1
Isaiah Akorita <akoritaisaiah@gmail.com>
2025-11-26 11:34:26 +0100

    revert deletion of pyct usage

--- datashader/__init__.py
+++ datashader/__init__.py
@@ -17,18 +17,19 @@ from pandas import __version__ as pandas_version
 if Version(pandas_version) >= Version('0.24.0'):
     from . import datatypes  # noqa (API import)
 
-# make pyct's example commands available if possible
+# make pyct's example/data commands available if possible
 from functools import partial
 try:
-    from pyct.cmd import copy_examples as _copy, examples as _examples
+    from pyct.cmd import copy_examples as _copy, fetch_data as _fetch, examples as _examples
     copy_examples = partial(_copy,'datashader')
+    fetch_data = partial(_fetch,'datashader')
     examples = partial(_examples,'datashader')
 except ImportError:
     def _missing_cmd(*args,**kw):
         return("install pyct to enable this command (e.g. `conda install pyct or "
                "`pip install pyct[cmd]`)")
-    _copy = _examples = _missing_cmd
+    _copy = _fetch = _examples = _missing_cmd
     def err():
         raise ValueError(_missing_cmd())
-    copy_examples = examples = err
-del partial, _examples, _copy
+    fetch_data = copy_examples = examples = err
+del partial, _examples, _copy, _fetch
