a0fabc9fe861e96b11147ee4c85767951bdaa2a2
Simon Høxbro Hansen <simon.hansen@me.com>
2026-01-05 20:18:10 +0100

    compat: Pandas 3 (#1473)

--- examples/conftest.py
+++ examples/conftest.py
@@ -29,10 +29,26 @@ if find_spec("dask") is not None:
         ]
 
 
-def pytest_sessionfinish(session, exitstatus):
-    # Can be removed when spatialpandas work with dask-expr
+def pytest_runtest_makereport(item, call):
+    """
+    Skip tests that fail because "the kernel died before replying to kernel_info"
+    this is a common error when running the example tests in CI.
 
-    from pytest import ExitCode
+    Inspired from: https://stackoverflow.com/questions/32451811
 
-    if exitstatus == ExitCode.NO_TESTS_COLLECTED:
-        session.exitstatus = ExitCode.OK
+    """
+    from _pytest.runner import pytest_runtest_makereport
+
+    tr = pytest_runtest_makereport(item, call)
+
+    if call.excinfo is not None:
+        msgs = [
+            "Kernel died before replying to kernel_info",
+            "Kernel didn't respond in 60 seconds",
+        ]
+        for msg in msgs:
+            if call.excinfo.type is RuntimeError and call.excinfo.value.args[0] in msg:
+                tr.outcome = "skipped"
+                tr.wasxfail = f"reason: {msg}"
+
+    return tr
