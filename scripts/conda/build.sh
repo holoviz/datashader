be9f2ccf3a46ddaf85a57b2a8b62d8283b18d54f
Simon Høxbro Hansen <simon.hansen@me.com>
2026-03-19 15:26:23 +0100

    ci: Update build and docs workflow (#1486)

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -9,6 +9,6 @@ python -m build --sdist .
 VERSION=$(python -c "import $PACKAGE; print($PACKAGE._version.__version__)")
 export VERSION
 
-conda build scripts/conda/recipe --no-anaconda-upload --no-verify -c conda-forge --package-format 1
+conda build scripts/conda/recipe --no-anaconda-upload --no-verify -c conda-forge --package-format 2
 
-mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.tar.bz2" dist
+mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.conda" dist
