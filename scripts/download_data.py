3f840be47771496cb57118f6f5a4235115bd6610
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-03-27 08:08:46 +0100

    ci: Add retry option to download data to avoid failures

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,31 +1,46 @@
+import sys
+import time
 from contextlib import suppress
+
 from packaging.version import Version
+
+
+def retry(func, *args, **kwargs):
+    for i in range(5):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            wait = 10 * 2**i
+            print(f"Attempt {i + 1} failed: {e}. Retrying in {wait}s...", file=sys.stderr)
+            time.sleep(wait)
+    return func(*args, **kwargs)
+
 
 with suppress(ImportError):
     import pyct.cmd
 
-    pyct.cmd.fetch_data(name="data", path="examples", datasets="datasets.yml")
+    retry(pyct.cmd.fetch_data, name="data", path="examples", datasets="datasets.yml")
 
 
 with suppress(ImportError):
     import bokeh
 
     # Replaced with bokeh_sampledata in 3.5
-    if Version(bokeh.__version__) < Version("3.5"):
+    if Version(bokeh.__version__).release < (3, 5, 0):
         import bokeh.sampledata
 
-        bokeh.sampledata.download()
+        retry(bokeh.sampledata.download)
 
 
 with suppress(ImportError):
     import geodatasets as gds
 
-    gds.get_path("geoda.natregimes")
-    gds.get_path("nybb")
-    gds.get_path("geoda health")
+    retry(gds.get_path, "geoda.natregimes")
+    retry(gds.get_path, "nybb")
+    retry(gds.get_path, "geoda health")
 
 
 with suppress(ImportError):
     import hvsampledata as hvs
 
-    path = hvs.download("nyc_taxi_remote")
+    retry(hvs.download, "nyc_taxi_remote")
